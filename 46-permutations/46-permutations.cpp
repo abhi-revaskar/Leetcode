8d232056d91de9361d7d316bdcdb0758e773775b
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-05-24 20:29:12 +0530

    Time: 771 ms (5.80%), Space: 145.3 MB (5.15%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> findperm(vector<vector<int>>& ans,vector<int>& ds,vector<int>& a,int i)
+   vector<vector<int>> findperm(vector<vector<int>>& ans,vector<int>& a,int i)
 {
   int n=a.size();
   if (n==1)
   {
-    ds.push_back(a[0]);
-    ans.push_back(ds);
+    ans.push_back({a[0]});
     return ans;
   }
   if (i>=n)
-  ans.push_back(ds);
+  ans.push_back(a);
   for (int j = i; j < n; j++)
   {
     swap(a[i],a[j]);
-    ds.push_back(a[i]);
-    findperm(ans,ds,a,i+1);
-    swap(a[i],a[j]);
-    ds.pop_back();
+    findperm(ans,a,i+1);
+    swap(a[i],a[j]);//backtrack
   }
   return ans;
 
 }
 vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        vector<vector<int>> perms = findperm(ans,ds,nums,0);
+        vector<vector<int>> perms = findperm(ans,nums,0);
         return perms;
     }
 };
