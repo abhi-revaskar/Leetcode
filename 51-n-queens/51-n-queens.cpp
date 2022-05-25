219c3a9afa6bcda3dc431c952f929773c3d6cad1
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-05-25 12:43:54 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (68.43%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,31 +1,7 @@
 class Solution {
 public:
-    bool canplace(int i,int j,int n,vector<string> temp)
-{
-  int k=i-1,l=j-1;
-  while (k>=0 && l>=0)
-  {
-    if(temp[k][l]=='Q') //left diagonal
-    return false;
-    k--;l--;
-  }
-  k=i-1;
-  while (k>=0)
-  {
-    if(temp[k][j]=='Q') //previous rows
-    return false;
-    k--;
-  }
-  k=i-1;l=j+1;
-  while (k>=0 && l<n)
-  {
-    if(temp[k][l]=='Q') //right diagonal
-    return false;
-    k--;l++;
-  }
-  return true;
-}
-void nqueen(vector<vector<string>> &ans,vector<string>& temp,int n,int i)
+    void nqueen(vector<vector<string>> &ans,vector<string>& temp,int n,int i,
+vector<int>& row,vector<int>& leftD,vector<int>& rightD)
 {
   if(i==n)
   {
@@ -34,23 +10,30 @@ void nqueen(vector<vector<string>> &ans,vector<string>& temp,int n,int i)
   }
   for (int j = 0; j < n; j++)
   {
-    if(canplace(i,j,n,temp))
+    if(row[j]==0 && leftD[j-i+n-1]==0 && rightD[i+j]==0)
     {
       temp[i][j]='Q';
-      nqueen(ans,temp,n,i+1);
+      row[j]=1;
+      leftD[j-i+n-1]=1;
+      rightD[i+j]=1;
+      nqueen(ans,temp,n,i+1,row,leftD,rightD);
       temp[i][j]='.';
+      row[j]=0;
+      leftD[j-i+n-1]=0;
+      rightD[i+j]=0;
     }  
   }
 }
  vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        string s(n,'.');
         vector<string> temp(n);
+        string s(n,'.');
         for (int i = 0; i < n; i++)
         {
           temp[i]=s;
         }
-        nqueen(ans,temp,n,0);
+        vector<int> row(n,0),leftD(2*n-1,0),rightD(2*n-1,0);
+        nqueen(ans,temp,n,0,row,leftD,rightD);
         return ans;
     }
 };
