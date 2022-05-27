a0c6016dc5787ada53bbdb211f0f0bbdaf4bf942
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-05-27 18:30:30 +0530

    Time: 70 ms (22.94%), Space: 89.4 MB (63.86%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -3,32 +3,29 @@ public:
    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n=nums1.size();
         int m=nums2.size();
-        if (n==0)
-        {
-          if(m&1)
-          return nums2[m/2];
-          else 
-          return double(nums2[(m-1)/2]+nums2[(m-1)/2+1])/2.0;
-        }
-        if (m==0)
-        {
-          if(n&1)
-          return nums1[n/2];
-          else 
-          return double(nums1[(n-1)/2]+nums1[(n-1)/2+1])/2.0;
-        }
-        int half=(n+m+1)/2;;
-        int l=max(0,half-m),h=min(half,n),l1,l2,r1,r2;
+        
+        if(n>m)
+        return findMedianSortedArrays(nums2,nums1);
+
+        int l=0,h=n;
         int mid;
-        while (l<=h)  
+        int half=(n+m+1)/2;
+        while (l<=h)
         {
           mid=(l+h)/2;
-          l1=mid-1;r1=mid;l2=half-mid-1;r2=half-mid;
-          if((l1<0?INT_MIN:nums1[l1])<=(r2==m?INT_MAX:nums2[r2]) && (l2<0?INT_MIN:nums2[l2])<=(r1==n?INT_MAX:nums1[r1]))
+          int cut1=mid;
+          int cut2=half-mid;
+
+          int l1=cut1==0?INT_MIN:nums1[cut1-1];
+          int l2=cut2==0?INT_MIN:nums2[cut2-1];
+          int r1=cut1==n?INT_MAX:nums1[cut1];
+          int r2=cut2==m?INT_MAX:nums2[cut2];
+
+          if(l1<=r2 && l2<=r1)
           {
-            break;
+            return (n+m)&1?max(l1,l2):((max(l1,l2)+min(r1,r2))/2.0);
           }
-          if(l1>=0 && r2<m && nums1[l1]>nums2[r2])
+          if(l1>r2)
           {
             h=mid-1;
           }
@@ -36,12 +33,8 @@ public:
           {
             l=mid+1;
           }
+
         }
-        // cout<<l1<<r1<<l2<<r2<<endl;
-        if((n+m)%2==0)
-        return double(max((l1<0?INT_MIN:nums1[l1]),(l2<0?INT_MIN:nums2[l2]))+min((r1==n?INT_MAX:nums1[r1]),(r2==m?INT_MAX:nums2[r2])))/2.0;
-        else
-        return max(l1<0?INT_MIN:nums1[l1],l2<0?INT_MIN:nums2[l2]);
-        
+        return -1;
     }
 };
