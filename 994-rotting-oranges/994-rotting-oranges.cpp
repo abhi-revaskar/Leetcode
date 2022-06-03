f0198c62880c9e3659023d4691a39da3b58abb5d
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-06-03 18:39:45 +0530

    Time: 15 ms (30.01%), Space: 13.3 MB (27.07%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int ans=0,flag=0;
+        queue<pair<int,int>> q;
+        vector<vector<int>> level(n,vector<int> (m,-1));
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    q.push({i,j});
+                    level[i][j]=0;
+                    flag=1;
+                }
+                if(grid[i][j]==1)
+                    flag=1;
+            }
+        }
+        if(!flag)
+            return 0;
+        if(q.empty())
+        return -1;
+        while (!q.empty())
+        {
+            auto t=q.front();
+            int currlevel=level[t.first][t.second];
+            int u=t.first-1,d=t.first+1,l=t.second-1,r=t.second+1;
+            if(u>=0 && grid[u][t.second]==1)
+            {
+                q.push({u,t.second});
+                grid[u][t.second]=2;
+                level[u][t.second]=currlevel+1;
+            }
+            if(d<n && grid[d][t.second]==1)
+            {
+                q.push({d,t.second});
+                grid[d][t.second]=2;
+                level[d][t.second]=currlevel+1;
+            }
+            if(l>=0 && grid[t.first][l]==1)
+            {
+                q.push({t.first,l});
+                grid[t.first][l]=2;
+                level[t.first][l]=currlevel+1;
+            }
+            if(r<m && grid[t.first][r]==1)
+            {
+                q.push({t.first,r});
+                grid[t.first][r]=2;
+                level[t.first][r]=currlevel+1;
+            }
+            q.pop();
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    return -1;
+                }
+                ans=max(ans,level[i][j]);
+            }
+        }
+        return ans;
+    }
+
+};
