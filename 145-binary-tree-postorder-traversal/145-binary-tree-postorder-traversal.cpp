662355a616395817403a38451b123d16a6162c18
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-06-07 11:43:50 +0530

    Time: 3 ms (55.70%), Space: 8.4 MB (86.81%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -11,27 +11,47 @@
  */
 class Solution {
 public:
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        if(!root)
-            return ans;
-        stack<TreeNode*> s1,s2;
-        s1.push(root);
-        while(!s1.empty())
-        {
-            TreeNode* t= s1.top();
-            s2.push(t);
-            s1.pop();
-            if(t->left)
-                s1.push(t->left);
-            if(t->right)
-                s1.push(t->right);
-        }
-        while(!s2.empty())
-        {
-            ans.push_back(s2.top()->val);
-            s2.pop();
-        }
-        return ans;
-    }
+    // vector<int> postorderTraversal(TreeNode* root) {
+    //     vector<int> ans;
+    //     if(!root)
+    //         return ans;
+    //     stack<TreeNode*> s1,s2;
+    //     s1.push(root);
+    //     while(!s1.empty())
+    //     {
+    //         TreeNode* t= s1.top();
+    //         s2.push(t);
+    //         s1.pop();
+    //         if(t->left)
+    //             s1.push(t->left);
+    //         if(t->right)
+    //             s1.push(t->right);
+    //     }
+    //     while(!s2.empty())
+    //     {
+    //         ans.push_back(s2.top()->val);
+    //         s2.pop();
+    //     }
+    //     return ans;
+    // }
+     vector<int> postorderTraversal(TreeNode* root) {
+         vector<int> ans;
+         if(!root)
+             return ans;
+         stack<TreeNode*> st;
+         st.push(root);
+         while(!st.empty())
+         {
+             TreeNode* t=st.top();
+             ans.push_back(t->val);
+             st.pop();
+             if(t->left)
+                 st.push(t->left);
+             if(t->right)
+                 st.push(t->right);
+         }
+         reverse(ans.begin(),ans.end());
+         return ans;
+         
+     }
 };
