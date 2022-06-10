732aacbcfd7af228ecce3ea77e07ae8ca7f34f9f
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-06-10 22:44:50 +0530

    Time: 13 ms (17.45%), Space: 16.9 MB (5.02%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -24,6 +24,38 @@ public:
         return p->val==q->val && solve(p->left,q->right) && solve(p->right,q->left);
     }
     bool isSymmetric(TreeNode* root) {
-        return solve(root->left,root->right);
+        // return solve(root->left,root->right);
+        stack<TreeNode*> s1,s2;
+        if(!root->left != !root->right)
+            return false;
+        if(!root->left)
+            return true;
+        if(root->left->val!=root->right->val)
+            return false;
+        s1.push(root->left);
+        s2.push(root->right);
+        while(!s1.empty() && !s1.empty())
+        {
+            TreeNode* t1=s1.top(),*t2=s2.top();
+            if(t1->val!=t2->val)
+                return false;
+            s1.pop();
+            s2.pop();
+            if(!t1->right != !t2->left || !t1->left != !t2->right)
+                return false;
+            if(t1->right && t2->left)
+            {
+                s1.push(t1->right);
+                s2.push(t2->left);
+            }
+            if(t1->left && t2->right)
+            {
+                s1.push(t1->left);
+                s2.push(t2->right);
+            }
+        }
+        if(!s1.empty() || !s2.empty())
+            return false;
+        return true;
     }
 };
