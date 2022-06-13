b72c1c582a2f677ad2da82b9c0382518489158d2
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-06-13 11:35:45 +0530

    Time: 510 ms (15.87%), Space: 226 MB (7.39%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -11,40 +11,22 @@
  */
 class Solution {
 public:
-    unordered_set<TreeNode*> s;
-    unordered_map<TreeNode*,int> mp;
-    int sum(TreeNode* root)
+    int ans=0;
+    vector<int> BstAndSum(TreeNode* root)
     {
         if(!root)
-            return 0;
-        mp[root]=root->val + sum(root->left)+sum(root->right);
-        return mp[root];
-    }
-    pair<int,int> bst(TreeNode* root)
-    {
-        if(!root)
-            return {INT_MAX,INT_MIN};
-        auto t1 = bst(root->left);
-        auto t2 = bst(root->right);
-        if(root->val > t1.second && root->val < t2.first)
+            return {INT_MAX,INT_MIN,0};
+        auto t1 = BstAndSum(root->left);
+        auto t2 = BstAndSum(root->right);
+        if(root->val > t1[1] && root->val < t2[0])
         {
-            s.insert(root);
-            // cout<<root->val<<endl;
-            pair<int,int> ans;
-            ans.first = min(t1.first,root->val);
-            ans.second = max(t2.second,root->val);
-            return ans;
+            ans=max(ans,root->val+t1[2]+t2[2]);
+            return {min(t1[0],root->val),max(t2[1],root->val),root->val+t1[2]+t2[2]};
         }
-        return {INT_MIN,INT_MAX};
+        return {INT_MIN,INT_MAX,root->val+t1[2]+t2[2]};
     }
     int maxSumBST(TreeNode* root) {
-        bst(root);
-        int ans = 0;
-        sum(root);
-        for(auto x:s)
-        {
-            ans=max(ans,mp[x]);
-        }
+        BstAndSum(root);
         return ans;
     }
 };
