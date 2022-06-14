4846a94419677b5798f5aa4e525b99b10ad3b22d
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-06-14 23:07:44 +0530

    Time: 28 ms (62.93%), Space: 14.7 MB (27.17%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>> &g,vector<bool> &todo,vector<bool> &done,int node)
+    bool dfs(vector<vector<int>> &g,vector<int> &t,vector<int> &v,int node)
     {
-        if (todo[node]) {
-            return false;
-        }
-        if (done[node]) {
-            return true;
-        }
-        todo[node] = done[node] = true;
-        for (int v : g[node]) {
-            if (!dfs(g, todo, done, v)) {
+        v[node]=1;
+        for (auto x : g[node]) {
+            if(x==node)
                 return false;
-            }
+            if(v[x]==1 && t[x]==0)
+                return false;
+            if (v[x]==0 && !dfs(g, t, v, x)) 
+                return false;
         }
-        todo[node] = false;
+        t[node]=1;
         return true;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> graph = buildGraph(numCourses,prerequisites);
-        vector<bool> v(numCourses,false),t(numCourses,false);
-        for(int i=0;i<numCourses;i++)
-        {
-            if(!v[i] && !dfs(graph,t,v,i))
-                return false;
-        }
-        return true;
-    }
-    vector<vector<int>> buildGraph(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> g(numCourses);
         for (auto p : prerequisites) {
             g[p[1]].push_back(p[0]);
         }
-        return g;
+        vector<int> v(numCourses,0),t(numCourses,0);
+        for(int i=0;i<numCourses;i++)
+        {
+            if(v[i]==0 && !dfs(g,t,v,i))
+                return false;
+        }
+        return true;
     }
 };
