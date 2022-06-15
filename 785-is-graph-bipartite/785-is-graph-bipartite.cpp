4cd5bcb129e59d74a2fee78b6fec42c53ba50d56
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-06-15 17:44:52 +0530

    Time: 47 ms (19.86%), Space: 13.5 MB (80.24%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -18,42 +18,40 @@ public:
     }
     bool isBipartite(vector<vector<int>>& graph) {
         vector<int> color(graph.size(),0);
-        for(int i = 0;i<graph.size();i++)
-        {
-            if(!color[i])
-            {
-                color[i]=1;
-                if(!dfs(graph,color,i))
-                    return false;
-            }
-        }
-        return true;
-        // queue<int> q;
-        // for(int i=0;i<graph.size();i++)
+        // for(int i = 0;i<graph.size();i++)
         // {
-        //     if(!v[i])
+        //     if(!color[i])
         //     {
-        //         q.push(i);
         //         color[i]=1;
-        //         v[i]=1;
-        //         while(!q.empty())
-        //         {
-        //             auto t = q.front();
-        //             for(auto x:graph[t])
-        //             {
-        //                 if(color[x]==color[t])
-        //                     return false;
-        //                 if(!v[x])
-        //                 {
-        //                     q.push(x);
-        //                     v[x]=1;
-        //                     color[x]=-color[t];
-        //                 }
-        //             }
-        //             q.pop();
-        //         }
+        //         if(!dfs(graph,color,i))
+        //             return false;
         //     }
         // }
         // return true;
+        queue<int> q;
+        for(int i=0;i<graph.size();i++)
+        {
+            if(!color[i])
+            {
+                q.push(i);
+                color[i]=1;
+                while(!q.empty())
+                {
+                    auto t = q.front();
+                    for(auto x:graph[t])
+                    {
+                        if(color[x]==color[t])
+                            return false;
+                        if(!color[x])
+                        {
+                            q.push(x);
+                            color[x]=-color[t];
+                        }
+                    }
+                    q.pop();
+                }
+            }
+        }
+        return true;
     }
 };
