090de59c76c050f76ff1ef161e2d71f235a29227
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-06-27 18:46:11 +0530

    Time: 704 ms (5.90%), Space: 82.9 MB (77.41%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int a[500002];
-        memset(a,0,sizeof(a));
-        for(auto x:nums)
+        for(int i=0;i<nums.size();i++)
         {
-            if(x>0 && x<=500000)
-                a[x]=1;
+            if(nums[i]<0)
+                nums[i]=0;
         }
-        for(int i=1;i<500002;i++)
-            if(a[i]==0)
+        for(int i=0 ;i<nums.size();i++)
+        {
+            int n = abs(nums[i]);
+            if(n>0 && n<=nums.size())
+                if(nums[n-1]!=0)
+                    nums[n-1] = -(abs(nums[n-1]));
+            else
+                 nums[n-1] = -n;
+        }
+        for(auto x:nums)
+            cout<<x<<" ";
+        for(int i=1;i<=nums.size();i++)
+            if(nums[i-1]>=0)
                 return i;
-        return 1;
+        return nums.size()+1;
     }
 };
