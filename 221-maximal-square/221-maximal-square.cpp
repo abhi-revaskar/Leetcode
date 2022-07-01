8ad59af1fa0d4332bb7a5afeebf3ea2b188bbc72
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-01 18:38:48 +0530

    Time: 361 ms (6.35%), Space: 16.2 MB (99.15%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& matrix) {
-        vector<vector<int>> m(matrix.size(),vector<int> (matrix[0].size()));
+    int maximalSquare(vector<vector<char>>& m) {
+        vector<int> dp;
         int ans = 0;
-        for(int i = 0;i<matrix.size();i++)
+        for(auto x:m[0])
+            dp.push_back(x-'0');
+        for(auto x:dp)
+            ans = max(ans,x);
+        int prev = dp[0];
+        for(int i =1;i<m.size();i++)
         {
-            for(int j = 0;j<matrix[0].size();j++)
-            {
-                m[i][j] = matrix[i][j]-'0';
-                ans = max(ans,m[i][j]);
-            }
-        }
-        vector<int> prev(m[0]),dp(matrix[0].size(),0);
-        for(int i = 1;i<m.size();i++)
-        {
-            int c = m[i][0];
-            dp=m[i];
+            ans = max(ans,m[i][0]-'0');
+            prev = dp[0];
+            dp[0] = m[i][0]-'0';
             for(int j = 1;j<m[0].size();j++)
             {
-                if(m[i][j])
+                if(m[i][j]=='1')
                 {
-                    int side = min(c,m[i-1][j]);
-                    side = min(prev[j-1],side);
-                    side++;
-                    ans = max(ans,side*side);                        
-                    dp[j]=side;
-                    c++;
-                    m[i][j]+=m[i-1][j];
+                    int t = dp[j];
+                    // cout<<dp[j]<<" "<<dp[j-1]<<" "<<prev<<endl;
+                    dp[j] = min(dp[j],min(dp[j-1],prev))+1;
+                    ans = max(ans,dp[j]*dp[j]);
+                    prev = t;
                 }
                 else
-                    c=0;
-                
-                // cout<<i<<":"<<dp[j]<<" ";
+                    dp[j]=0;
+                cout<<dp[j]<<" ";
             }
-            // cout<<endl;
-            prev=dp;
+            cout<<endl;
         }
         return ans;
     }
