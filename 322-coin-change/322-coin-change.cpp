d5846ee88a4380217bb8a10eb4b878681d710a98
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-01 19:30:40 +0530

    Time: 289 ms (9.28%), Space: 49.8 MB (7.94%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -15,11 +15,21 @@ public:
             ans = min(ans,1+solve(i,coins,amt-coins[i]));
         return dp[i][amt]=ans;
     }
-    int coinChange(vector<int>& coins, int amount) {
-        memset(dp,-1,sizeof(dp));
-        int ans = solve(0,coins,amount);
-        if(ans>10000)
-            return -1;
-        return ans;
+    int coinChange(vector<int>& coins, int amt) {
+        // memset(dp,-1,sizeof(dp));
+        // int ans = solve(0,coins,amount);
+        // if(ans>10000)
+        //     return -1;
+        vector<vector<int>> dp(coins.size()+1,vector<int>(amt+1,10001));
+        dp[0][0] = 0;
+        for(int i=1;i<=coins.size();i++)
+        {
+            dp[i][0]=0;
+            for(int j=1;j<=amt;j++)
+            {
+                dp[i][j] = min(dp[i-1][j],coins[i-1]>j?INT_MAX:1+dp[i][j-coins[i-1]]);
+            }
+        }
+        return dp[coins.size()][amt]>10000?-1:dp[coins.size()][amt];
     }
 };
