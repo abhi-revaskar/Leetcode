45b67beeb89070cce3469cf9f116c45e6d41388a
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-01 12:20:13 +0530

    Time: 23 ms (45.12%), Space: 9.5 MB (64.10%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& w) {
-        unordered_map<string,int> mp;
-        for(auto x:w)
-        {
-            mp[x]=1;
-        }
-        int dp[s.length()+1];
-        memset(dp,0,sizeof(dp));
-        dp[0]=1; //base condition
-        for(int i=1;i<=s.length();i++)
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> set;
+        for(auto x:wordDict)
+            set.insert(x);
+        vector<int> dp(s.size()+1,0);
+        dp[0]=1;
+        for(int i=1;i<=s.size();i++)
         {
             for(int j=i-1;j>=0;j--)
             {
-                if(dp[j]==1) //if a valid segement ends here s[0..j]
+                if(dp[j]==1 && set.find(s.substr(j,i-j))!=set.end())
                 {
-                    if(mp.find(s.substr(j,i-j))!=mp.end()) // if the substr s[j..i] is present in the dict
-                    {
-                        dp[i]=1;
-                        break; // found a valid segment ending at this position.
-                    }
+                    dp[i]=1;
+                    break;
                 }
             }
         }
-        return dp[s.length()];
+        // for(auto x:dp)
+        //     cout<<x<<" ";
+        return dp[s.size()];
     }
 };
