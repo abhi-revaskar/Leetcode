702a16b91489b7cd154f5293205e6b538821f937
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-02 19:02:41 +0530

    Time: 10 ms (91.19%), Space: 10.2 MB (97.76%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        // vector<int> dp(nums.size(),1);
-        // int ans = 1;
-        // for(int i=1;i<nums.size();i++)
-        // {
-        //     for(int j=i-1;j>=0;j--)
-        //     {
-        //         if(nums[j]<nums[i])
-        //             dp[i] = max(dp[i],dp[j]+1);
-        //     }
-        //     ans = max(ans,dp[i]);
-        // }
-        // return ans;
-        vector<int> lis{nums[0]};
-        for(int i=0;i<nums.size();i++)
+        vector<int> is;
+        is.push_back(nums[0]);
+        for(int i = 1;i<nums.size();i++)
         {
-            if(lis.back()<nums[i])
-                lis.push_back(nums[i]);
+            if(is.back()<nums[i])
+                is.push_back(nums[i]);
             else
-            {
-                int idx = lower_bound(lis.begin(),lis.end(),nums[i]) - lis.begin();
-                lis[idx] = nums[i];
-            }
-            
+                // cout<<upper_bound(is.begin(),is.end(),nums[i])-is.begin();
+                is[lower_bound(is.begin(),is.end(),nums[i])-is.begin()]=nums[i];
         }
-        return lis.size();
+        return is.size();
     }
 };
