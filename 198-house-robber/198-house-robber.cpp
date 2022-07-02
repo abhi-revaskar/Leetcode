a77ec6fab6156dd7fc563d8c8c6c67be94847c3b
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-02 14:55:26 +0530

    Time: 3 ms (53.94%), Space: 7.6 MB (94.78%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        vector<int> profit(nums.size()+1);
-        profit[0]=0;
-        profit[1]=nums[0];
+        int far =0;
+        int near=nums[0];
         for(int i =2;i<=nums.size();i++)
         {
-            profit[i]=max(nums[i-1]+profit[i-2],profit[i-1]);
+            int curr =max(nums[i-1]+far,near);
+            far = near;
+            near = curr;
         }
-        return profit.back();
+        return near;
     }
 };
