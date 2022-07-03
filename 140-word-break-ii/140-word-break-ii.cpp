a54b585f8285b987239f91fe83e0fe668e7be8b4
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-03 17:45:17 +0530

    Time: 3 ms (49.71%), Space: 7.2 MB (82.73%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.cpp
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     unordered_set<string> set;
-    vector<string> solve(int i,string s,vector<string> &dict)
+    vector<string> solve(string s,vector<string> &dict)
     {
         vector<string> ans;
-        for(int idx = i;idx<s.length();idx++)
+        for(int idx = 0;idx<s.length();idx++)
         {
-            string prefix = s.substr(i,idx-i+1);
+            string prefix = s.substr(0,idx+1);
             if(set.find(prefix)!=set.end())
             {
                 if(idx==s.length()-1)
@@ -14,7 +14,7 @@ public:
                     ans.push_back(prefix);
                     return ans;
                 }
-                vector<string> temp = solve(0,s.substr(idx+1),dict);
+                vector<string> temp = solve(s.substr(idx+1),dict);
                 for(auto x:temp)
                 {
                     ans.push_back(prefix+" "+x);
@@ -24,9 +24,8 @@ public:
         return ans;
     }
     vector<string> wordBreak(string s, vector<string>& dict) {
-        string ds = "";
         for(auto x:dict)
             set.insert(x);
-        return solve(0,s,dict);
+        return solve(s,dict);
     }
 };
