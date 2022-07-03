1a2d7d934be7a37e80b783a197762cdccb0f1ce1
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-03 13:52:19 +0530

    Time: 486 ms (46.78%), Space: 362 MB (5.32%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -5,11 +5,11 @@ public:
         vector<vector<int>> dp(1001,vector<int> (1001,1));
         for(int i = 0;i<nums.size();i++)
         {
-            for(int j=i+1;j<nums.size();j++)
+            for(int j=0;j<i;j++)
             {
-                int diff = nums[j] - nums[i]+500;
-                dp[j][diff] = 1+dp[i][diff];
-                ans = max(ans,dp[j][diff]);
+                int diff = nums[i] - nums[j]+500;
+                dp[i][diff] = 1+dp[j][diff];
+                ans = max(ans,dp[i][diff]);
             }
         }
         return ans;
