e2b205ee784e716e8575ffd8726a18647aab46de
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-03 11:28:18 +0530

    Time: 1136 ms (5.02%), Space: 142.4 MB (99.61%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -1,29 +1,23 @@
 class NumMatrix {
 public:
-    vector<vector<int>> a,sum;
-    int n,m;
+    vector<vector<int>> a;
     NumMatrix(vector<vector<int>>& matrix) {
-        a=matrix;
-        n=a.size();
-        m=a[0].size();
-        sum.resize(n);
-        for(int i=0 ; i<n ;i++)
+        int n = matrix.size(),m = matrix[0].size();
+        for(int i=0;i<n;i++)
         {
-            int s=0;
-            for(int j=0;j<m;j++)
+            int sum = 0;
+            for(int j = 1;j<m;j++)
             {
-                s+=a[i][j];
-                sum[i].push_back(s);
+                matrix[i][j] += matrix[i][j-1];
             }
         }
+        a = matrix;
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int ans=0;
-        for(int i=row1;i<=row2;i++)
-        {
-            ans+=sum[i][col2]-(col1==0?0:sum[i][col1-1]);
-        }
+        int ans = 0;
+        for(int i = row1;i<=row2;i++)
+            ans+=a[i][col2]-(col1>0?a[i][col1-1]:0);
         return ans;
     }
 };
