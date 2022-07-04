ff63f7f029f31bb9d856013b881418a8897aa599
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-04 17:37:46 +0530

    Time: 11 ms (84.26%), Space: 9.1 MB (21.03%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    
-    int minDistance(string w1, string w2) {
-        int n=w1.length(),m=w2.length();
-        vector<int> curr(m+1),prev(m+1);
-        for(int j=0;j<=m;j++)
-            prev[j]=j;
-        for(int i=1;i<n+1;i++)
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size()+1,vector<int> (word2.size()+1,0));
+        for(int i = 0;i<=word1.size();i++)
         {
-            curr[0]=i;
-            for(int j=1;j<m+1;j++)
+            dp[i][0]=i;
+            for(int j = 1;j<=word2.size();j++)
             {
-                if(w1[i-1]==w2[j-1])
-                    curr[j] = prev[j-1];
+                if(i==0)
+                    dp[i][j]=j;
+                else if(word1[i-1]!=word2[j-1])
+                    dp[i][j] = 1+min(dp[i-1][j],min(dp[i-1][j-1],dp[i][j-1]));
                 else
-                    curr[j]=1+min(prev[j],min(curr[j-1],prev[j-1]));
+                    dp[i][j] = dp[i-1][j-1];
             }
-            prev=curr;
         }
-            return prev[m];
+        return dp.back().back();
     }
 };
