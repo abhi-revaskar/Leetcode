4bec8294f26f28ef9964c5e59e42c75b1478ca25
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-06 18:07:33 +0530

    Time: 4 ms (52.45%), Space: 5.8 MB (80.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     int fib(int n) {
         int a=0,b=1,c;
-        if(n==0)
-            return a;
-        if(n==1)
-            return b;
+        if(n==0 || n==1)
+            return n;
         for(int i=2;i<=n;i++)
         {
             c=a+b;
