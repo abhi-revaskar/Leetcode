3247b0edb38b23575c15e1d3a6c9dcbf717da91e
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-06 15:25:14 +0530

    Time: 453 ms (74.65%), Space: 8.4 MB (87.21%) - LeetHub

---
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dp[302][302];
+    int solve(int i,int j,vector<int> &nums)
+    {
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int ans = 0;
+        for(int k=i+1;k<j;k++)
+        {
+            ans = max(ans,solve(i,k,nums)+solve(k,j,nums)+nums[k]*nums[i]*nums[j]);
+        }
+        return dp[i][j] = ans;
+    }
+    int maxCoins(vector<int>& nums) {
+        nums.push_back(1);
+        nums.push_back(1);
+        for(int i=nums.size()-2;i>0;i--)
+        {
+            swap(nums[i],nums[i-1]);
+        }
+        memset(dp,-1,sizeof(dp));
+        return solve(0,nums.size()-1,nums);
+        return 0;
+    }
+};
