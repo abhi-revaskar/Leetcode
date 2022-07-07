895486d1270ede8b0e9d2bf4270fdbeffd078bc5
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-07 18:14:48 +0530

    Time: 3 ms (85.12%), Space: 6.2 MB (96.38%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -19,8 +19,40 @@ public:
         return dp[s1.length()][s2.length()]= ans;
     }
     bool isInterleave(string s1, string s2, string s3) {
-        memset(dp,-1,sizeof(dp));
-        return solve(s1,s2,s3);
-           
+        // memset(dp,-1,sizeof(dp));
+        // return solve(s1,s2,s3);
+        if((s1.length()+s2.length())!=s3.length())
+            return false;
+        int n = s1.length(),m=s2.length();
+        vector<int> prev(m+1,0),dp(m+1,0);
+        prev[m] = 1;
+        for(int i=m-1;i>=0;i--)
+        {
+            if(s3[n+i]==s2[i])
+                prev[i]=prev[i+1];
+        }
+        // for(auto x:prev)
+        //     cout<<x<<" ";
+        // cout<<endl;
+        for(int i=n-1;i>=0;i--)
+        {
+            dp[m]=((s3[m+i]==s1[i])?prev[m]:0);
+            for(int j=m-1;j>=0;j--)
+            {
+            
+                dp[j]=0;
+                if(s1[i]==s3[j+i])
+                    dp[j]=dp[j]||prev[j];
+                if(s2[j]==s3[j+i])
+                    dp[j]=dp[j]||dp[j+1];
+                // if(i==0 && j==0)
+                // cout<<s1[i]<<" "<<s2[j]<<" "<<s3[i+j]<<" "<<dp[j]<<endl;
+            }
+            // for(auto x:dp)
+            //     cout<<x<<" ";
+            // cout<<endl;
+            prev=dp;
+        }
+        return prev[0];
     }
 };
