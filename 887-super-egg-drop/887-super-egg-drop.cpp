863b11963d2e1495a03cc8bf01d8868da482f0bd
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-07 12:15:34 +0530

    Time: 57 ms (83.57%), Space: 10.4 MB (72.93%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     int dp[101][10001];
-    int superEggDrop1(int k, int n) {
+    int solve(int k,int n)
+    {
         if(dp[k][n]!=-1)
             return dp[k][n];
-        if(n==1 || k==1 || n==0)
-            return dp[k][n] = n;
+        if(k==1 || n==1 || n==0)
+            return dp[k][n]= n;
         int ans = INT_MAX;
         int l=1,h=n,mid;
         while(l<=h)
         {
             mid = (l+h)/2;
-            int t1=superEggDrop1(k-1,mid-1); //egg broke
-            int t2=superEggDrop1(k,n-mid); //egg not broke
-            int t = 1+max(t1,t2);
-            ans=min(ans,t);
-            if(t1<t2)
-                l=mid+1;
-            else
+            int breaks=solve(k-1,mid-1);
+            int doesnt = solve(k,n-mid);
+            ans = min(ans,max(breaks,doesnt));
+    
+            if(breaks>doesnt)
                 h=mid-1;
+            else
+                l=mid+1;
         }
-        return dp[k][n] = ans;
+        return dp[k][n]= ans+1;
     }
     int superEggDrop(int k, int n) {
         memset(dp,-1,sizeof(dp));
-        return superEggDrop1(k,n);
+        return solve(k,n);
     }
 };
