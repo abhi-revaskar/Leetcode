fec350b385e07aa48aa135148b60528d7df15228
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-10 18:35:16 +0530

    Time: 4 ms (83.90%), Space: 7.2 MB (75.34%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,58 +1,27 @@
 class Solution {
 public:
-    void revword(string &s,int i,int j)
-{
-    while (i<j)
-    {
-        swap(s[i],s[j]);
-        i++;j--;
+    string reverseWords(string s) {
+        string w = "";
+        string ss = "";
+        for(auto x:s)
+        {
+            if(x!=' ')
+                w+=x;
+            else if(!w.empty())
+            {
+                reverse(w.begin(),w.end());
+                ss+=w+' ';
+                w="";
+            }
+        }
+        if(!w.empty())
+        {
+            reverse(w.begin(),w.end());
+            ss+=w;
+        }
+        if(ss.back()==' ')
+            ss.erase(ss.length()-1);
+        reverse(ss.begin(),ss.end());
+        return ss;
     }
-}
-string reverseWords(string &s) {
-    int i=0,j=0,n=s.length();
-    while (s[i]==' ')
-    {
-        i++;
-    }
-    j=i;
-    while (j<n)
-    {
-        while (j<n-1 && s[j+1]!=' ')
-        {
-            j++;
-        }
-        revword(s,i,j);
-        j++;
-        while (j<n && s[j]==' ')
-        {
-            j++;
-        }
-        i=j;
-    }
-    revword(s,0,n-1);
-    i=j=0;
-    while (true)
-    {
-        while (j<n && s[j]==' ')
-        {
-            j++;
-        }
-        if(j==n)
-        {
-            s.resize(i-1);
-            break;
-        }
-        while (j<n && s[j]!=' ')
-        {
-            s[i++]=s[j++];
-        }
-        if(j==n) 
-        {
-            s.resize(i);
-            break;
-        }
-        s[i++]=s[j];
-    }
-    return s;
-}
 };
