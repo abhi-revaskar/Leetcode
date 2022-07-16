0a3b247e6198829922c51fb7aadcd3ea9067584b
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-16 11:15:59 +0530

    Time: 39 ms (38.11%), Space: 15.3 MB (14.95%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> dfs;
+    void solve(int i,vector<vector<int>> &adj,vector<int> &vis,vector<int> &trav)
+    {
+        // cout<<i<<endl;
+        vis[i] = 1;
+        for(auto x:adj[i])
+        {
+            if(!vis[x])
+                solve(x,adj,vis,trav);
+            else if(!trav[x])
+            {
+                dfs.push_back(-1);
+                return;
+            }
+        }
+        trav[i]=1;
+        dfs.push_back(i);
+    }
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(n);
+        for(auto x:prerequisites)
+        {
+            adj[x[1]].push_back(x[0]);
+        }
+        vector<int> vis(n,0),trav(n,0);
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+                solve(i,adj,vis,trav);
+        }
+        reverse(dfs.begin(),dfs.end());
+        // for(auto x:dfs)
+        //     cout<<x<<" ";
+        vector<int> ans;
+        for(auto x:dfs)
+            if(x==-1)
+                return ans;
+        return dfs;
+    }
+};
