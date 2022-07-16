bfbe67c8cda1b9fd366c0fc4f8c7022532745687
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-16 17:57:48 +0530

    Time: 23 ms (41.24%), Space: 9.9 MB (77.08%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,15 +1,5 @@
 class Solution {
 public:
-    void dfs2(int i,int j,vector<vector<char>> &board)
-    {
-        if(min(i,j)<0 || i==board.size() || j==board[0].size() || board[i][j]!='O')
-            return;
-        board[i][j] = 'X';
-        dfs2(i+1,j,board);
-        dfs2(i-1,j,board);
-        dfs2(i,j+1,board);
-        dfs2(i,j-1,board);
-    }
     void dfs(int i,int j,vector<vector<char>> &board)
     {
         if(min(i,j)<0 || i==board.size() || j==board[0].size() || board[i][j]!='O')
@@ -33,27 +23,16 @@ public:
                     
             }
         }
-        // print(board);
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(board[i][j]=='O')
                 {
-                    dfs2(i,j,board);
+                    board[i][j]='X';
                 }
-                    
-            }
-        }
-        // print(board);
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
                 if(board[i][j]=='N')
-                {
                     board[i][j]='O';
-                }
                     
             }
         }
