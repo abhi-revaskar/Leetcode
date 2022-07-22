cee799880ac7a09fb87cd144bdb7132c89e0971f
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-22 11:02:31 +0530

    Time: 4 ms (89.73%), Space: 12.8 MB (38.74%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -11,33 +11,23 @@
  */
 class Solution {
 public:
-    pair<TreeNode*,TreeNode*> solve(TreeNode* root)
-    {
-        if(!root->left && !root->right)
-            return {root,root};
-        if(!root->left)
-        {
-            pair<TreeNode*,TreeNode*> t= solve(root->right);
-            root->right = t.first;
-            return {root,t.second};
-        }
-        if(!root->right)
-        {
-            auto t= solve(root->left);
-            root->right = t.first;
-            root->left = NULL;
-            return {root, t.second};
-        }
-        auto t1 = solve(root->left);
-        auto t2 = solve(root->right);
-        root->right = t1.first;
-        t1.second->right = t2.first;
-        root->left = NULL;
-        return {root, t2.second};
-    }
     void flatten(TreeNode* root) {
         if(!root)
             return;
-        solve(root);
+        TreeNode* temp = root;
+        while(temp)
+        {
+            if(!temp->left)
+                temp=temp->right;
+            else
+            {
+                TreeNode* pred=temp->left;
+                while(pred->right)
+                    pred=pred->right;
+                pred->right = temp->right;
+                temp->right = temp->left;
+                temp->left = NULL;
+            }
+        }
     }
 };
