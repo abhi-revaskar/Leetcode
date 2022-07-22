eac3c3d9ffe3cd85770770cea3038c866473e0d5
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-22 11:53:24 +0530

    Time: 13 ms (60.80%), Space: 17 MB (99.99%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -12,22 +12,31 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        long long ans=1;
-        queue<pair<TreeNode*,long long>> q;
-        q.push({root,0});
+        int ans = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        root->val = 0;
         while(!q.empty())
         {
-            long long min=q.front().second;
-            long long curr = q.size();
-            while(curr--)
+            int mini = INT_MAX;
+            ans = max(ans,q.back()->val-q.front()->val+1);
+            int size = q.size();
+            while(size--)
             {
-                auto t=q.front();
-                ans=max(ans,t.second-min+1);
+                auto top = q.front();
                 q.pop();
-                if(t.first->left)
-                q.push({t.first->left,2*(t.second-min)+1});
-                if(t.first->right)
-                q.push({t.first->right,2*(t.second-min)+2});                
+                if(top->left)
+                {
+                    mini = min(mini,top->val);
+                    top->left->val = (top->val-mini)*2+1;
+                    q.push(top->left);
+                }
+                if(top->right)
+                {
+                    mini = min(mini,top->val);
+                    top->right->val = (top->val-mini)*2+2;
+                    q.push(top->right);
+                }
             }
         }
         return ans;
