0b3a0ab50cca26394e55d320227a3673d1bcca08
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-23 14:51:08 +0530

    Time: 7 ms (17.95%), Space: 7.3 MB (49.13%) - LeetHub

---
+++ 355-design-twitter/355-design-twitter.cpp
@@ -0,0 +1,53 @@
+class Twitter {
+public:
+    int time;
+    unordered_map<int,unordered_set<int>> followers,following;
+    unordered_map<int,vector<pair<int,int>>> tweets; // userId : [[time,tweet]]
+    Twitter() {
+        time=0;
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        time--;
+        tweets[userId].push_back({time,tweetId});
+        following[userId].insert(userId);
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        vector<int> ans;
+        int k=10;
+        set<pair<int,int>> feed;
+        for(auto followee:following[userId])
+        {
+            for(auto tweet:tweets[followee])
+                feed.insert(tweet);
+        }
+        for(auto tweet:feed)
+            if(k)
+            {
+                ans.push_back(tweet.second);
+                k--;
+            }
+        return ans;
+            
+    }
+    
+    void follow(int followerId, int followeeId) {
+        followers[followeeId].insert(followerId);
+        following[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        followers[followeeId].erase(followerId);
+        following[followerId].erase(followeeId);
+    }
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
