7a2cf62a3768b742cfd3224e0248211566a69040
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-26 11:51:11 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (44.28%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,66 +1,29 @@
 class Solution {
 public:
-   bool backspaceCompare(string s, string t) {
-  int n=s.length(),m=t.length();
-  int p1=n-1,p2=m-1,c1=0,c2=0;
-  while (p1>=0 && p2>=0)
-  {
-    
-    if(s[p1]=='#')
-    {
-      c1++;
-      p1--;
+    bool backspaceCompare(string s, string t) {
+        stack<char> st1,st2;
+        for(auto x:s)
+        {
+            if(x!='#')
+                st1.push(x);
+            else if(!st1.empty())
+                st1.pop();
+        }
+        for(auto x:t)
+        {
+            if(x!='#')
+                st2.push(x);
+            else if(!st2.empty())
+                st2.pop();
+        }
+        if(st1.size()!=st2.size())
+            return false;
+        while(!st1.empty() && !st2.empty())
+        {
+            if(st1.top()!=st2.top())
+                return false;
+            st1.pop(),st2.pop();
+        }
+        return true;
     }
-    else if(t[p2]=='#')
-    {
-      c2++;
-      p2--;
-    }
-    else if(!c1 && !c2 && s[p1]!=t[p2])
-    return false;
-    else if(c1)
-    {
-      c1--;
-      p1--;
-    }
-    else if(c2)
-    {
-      c2--;
-      p2--;
-    }
-    else
-    {
-      c1=max(0,c1-1);
-      c2=max(0,c2-1);
-      p1--;
-      p2--;
-    }
-  }
-
-  while (p1>=0)
-  {
-    if(c1<0)
-    return false;
-    
-    if(s[p1]=='#')
-    c1++;
-    else
-    c1--;
-    p1--;
-  }
-  while (p2>=0)
-  {
-    if(c1<0)
-    return false;
-    
-    if(t[p2]=='#')
-    c2++;
-    else
-    c2--;
-    p2--;
-  }
-  if(c1<0 || c2<0)
-  return false;
-  return true;
-  }
 };
