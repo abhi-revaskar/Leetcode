0c1da8af8da144b16e773d2391989f4b19ec49b4
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-27 12:44:34 +0530

    Time: 7 ms (66.09%), Space: 8.7 MB (46.40%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    int dp[41][41];
-    int solve(int i,int j,vector<int> &arr)
-    {
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if(i==j)
-            return 0;
-        if(i+1==j)
-            return arr[i]*arr[j];
-        int ans = INT_MAX,fmax = INT_MIN,smax = INT_MIN,n=arr.size();
-        vector<int> first(arr.size()),second(arr.size());
-        for(int k=i;k<=j;k++)
-        {            
-            first[k] = fmax=max(fmax,arr[k]);
-        }
-        for(int k=j;k>=i;k--)
-        {            
-            second[k] = smax=max(smax,arr[k]);
-        }
-        for(int k=i;k<j;k++)
-        {
-            int left=solve(i,k,arr);
-            int right=solve(k+1,j,arr);
-            ans = min(ans,left+right+first[k]*second[k+1]);
-            // cout<<k<<" "<<left<<" "<<right<<" "<<first[k]*second[k+1]<<endl;
-        }
-        // cout<<i<<" "<<j<<ans;
-        return dp[i][j] = ans;
-        
-    }
     int mctFromLeafValues(vector<int>& arr) {
-        memset(dp,-1,sizeof(dp));
-        return solve(0,arr.size()-1,arr);
+        vector<int> nge(arr.size()),pge(arr.size());
+        stack<int> st;
+        st.push(INT_MAX);
+        for(int i=arr.size()-1;i>=0;i--)
+        {
+            while(st.top()<arr[i])
+                st.pop();
+            nge[i] = st.top();
+            st.push(arr[i]);
+        }
+        st = stack<int> ();
+        st.push(INT_MAX);
+        for(int i=0;i<arr.size();i++)
+        {
+            while(st.top()<=arr[i])
+                st.pop();
+            pge[i] = st.top();
+            st.push(arr[i]);
+        }
+        int ans = 0;
+        for(int i=0;i<arr.size();i++)
+        {
+            if(nge[i]!=INT_MAX || pge[i]!=INT_MAX)
+                ans+=min(nge[i],pge[i])*arr[i];
+        }
+        return ans;
     }
 };
