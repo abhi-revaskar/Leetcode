e0432aa200f371568942adf45c2d85426481f7fa
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-07-28 17:49:30 +0530

    Time: 195 ms (89.14%), Space: 60.8 MB (67.54%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        unordered_set<int> judge;
-        int trusters = 0;
+        vector<int> degree(n+1,0);
+        for(auto &x:trust)
+        {
+            degree[x[1]]++;
+            degree[x[0]]--;
+        }
         for(int i=1;i<=n;i++)
-            judge.insert(i);
-        for(auto &x:trust)
-            judge.erase(x[0]);
-        if(judge.size()==0)
-            return -1;
-        int j = *(judge.begin());
-        for(auto &x:trust)
-            if(x[1]==j)
-                trusters++;
-        return (trusters==n-1)?j:-1;
+            if(degree[i]==n-1)
+                return i;
+        return -1;
     }
 };
