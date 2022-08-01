ba3dc1aa927fdb223a5f519940a026c8c4ec99ed
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-01 18:21:51 +0530

    Time: 535 ms (45.54%), Space: 126.4 MB (42.81%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        vector<vector<pair<int,int>>> adj(n);
-        vector<int> dist(n,INT_MAX);
-        dist[headID]=0;
+        vector<int> time(n,0);
+        vector<vector<int>> adj(n);
         for(int i=0;i<n;i++)
         {
             if(manager[i]!=-1)
-                adj[manager[i]].push_back({i,informTime[manager[i]]});
+                adj[manager[i]].push_back(i);
         }
-        int ans = 0;
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
-        pq.push({0,headID});
-        while(!pq.empty())
+        queue<int> q;
+        q.push(headID);
+        while(!q.empty())
         {
-            int d = pq.top()[0],node=pq.top()[1];
-            pq.pop();
+            int node = q.front();q.pop();
             for(auto x:adj[node])
             {
-                if(dist[x.first]>d+x.second)
-                {
-                    dist[x.first] = d+x.second;
-                    pq.push({dist[x.first],x.first});
-                }
+                time[x]+=time[node]+informTime[node];
+                q.push(x);
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            if(i!=headID)
-                ans = max(ans,dist[i]);
-        }
+        int ans = 0;
+        for(auto x:time)
+            ans = max(ans,x);
         return ans;
-        
     }
 };
