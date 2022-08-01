9ff94440ea01c57d0b463f1834e997e60d99be44
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-01 17:33:59 +0530

    Time: 93 ms (83.17%), Space: 12.6 MB (56.29%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,48 +1,42 @@
 class Solution {
 public:
-    int solve(int i,vector<vector<pair<int,int>>> &adj,int thresh)
-    {
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
-        vector<int> dist(adj.size(),INT_MAX);
-        dist[i]=0;
-        pq.push({0,i});
-        while(!pq.empty())
-        {
-            int d = pq.top()[0],node=pq.top()[1];
-            pq.pop();
-            for(auto nbr:adj[node])
-            {
-                if(dist[nbr.first]>d+nbr.second)
-                {
-                    dist[nbr.first] = d+nbr.second;
-                    pq.push({dist[nbr.first],nbr.first});
-                }
-            }
-        }
-        int cities = 0;
-        for(auto x:dist)
-            if(x<=thresh)
-                cities++;
-        return cities;
-    }
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<pair<int,int>>> adj(n);
+    int findTheCity(int n, vector<vector<int>>& edges, int thresh) {
+        vector<vector<int>> w(n,vector<int>(n,10001));
         for(auto x:edges)
         {
-            adj[x[0]].push_back({x[1],x[2]});
-            adj[x[1]].push_back({x[0],x[2]});
+            w[x[0]][x[1]]=x[2];
+            w[x[1]][x[0]]=x[2];
         }
-        int ans_node=0,mincities=INT_MAX;
+        for(int i=0;i<n;i++)
+            w[i][i]=0;
+        for(int k=0;k<n;k++)
+        {
+            // cout<<"k: "<<k<<endl;
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    w[i][j] = min(w[i][j],w[i][k]+w[k][j]);
+                    // cout<<w[i][j]<<" ";
+                }
+                // cout<<endl;
+            }
+            // cout<<endl;
+            
+        }
+        int ans_node=0,minnbrs=INT_MAX;
         for(int i=0;i<n;i++)
         {
-            int nodes_at_thresh = solve(i,adj,distanceThreshold);
-            if(nodes_at_thresh<=mincities)
+            int nbrs = 0;
+            for(auto nodes:w[i])
+                if(nodes<=thresh)
+                    nbrs++;
+            if(nbrs<=minnbrs)
             {
-                mincities=nodes_at_thresh;
+                minnbrs=nbrs;
                 ans_node=i;
             }
         }
         return ans_node;
-        
     }
 };
