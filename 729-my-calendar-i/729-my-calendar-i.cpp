26cea398227215cf5eb0afc6dbefa5e3cb75c3fc
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-03 13:04:55 +0530

    Time: 130 ms (73.82%), Space: 44.6 MB (6.38%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,17 +1,38 @@
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
+
+
+
+typedef tree<pair<int,int>, null_type, less_equal<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 class MyCalendar {
 public:
-    vector<vector<int>> lists;
+    ordered_set s;
     MyCalendar() {
         
     }
     
     bool book(int start, int end) {
-        for(auto x:lists)
+        if(s.empty())
         {
-            if(x[1]>start && x[0]<end)
+            s.insert({start,end});
+            return true;
+        }
+        int idx = s.order_of_key({start,end});
+        if(idx>0)
+        {
+            auto prev = s.find_by_order(idx-1);
+            if(prev->second>start)
                 return false;
         }
-        lists.push_back({start,end});
+        if(idx<s.size())
+        {
+            auto next = s.find_by_order(idx);
+            if(next->first<end)
+                return false;
+        }
+        s.insert({start,end});
         return true;
     }
 };
