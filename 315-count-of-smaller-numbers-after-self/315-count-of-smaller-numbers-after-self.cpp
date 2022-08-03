9f2a6a753eb6ddc0ae1d6f5aa009771cc3afbcc2
abhi-revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-03 13:12:50 +0530

    Time: 998 ms (40.36%), Space: 169.9 MB (61.66%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,54 +1,21 @@
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
+
+
+
+typedef tree<pair<int,int>, null_type, less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 class Solution {
-private:
-    void merge_sort(int start, int end, vector<pair<int, int>>& nums, vector<int>& indices, vector<pair<int, int>>& temp) {
-        if(start >= end)
-            return;
-        
-        int mid = start + (end - start) / 2;
-        
-        merge_sort(start, mid, nums, indices, temp);
-        merge_sort(mid + 1, end, nums, indices, temp);
-        
-        int left = start, right = mid + 1;
-        int idx = start;
-        int nRightLessThanLeft = 0;
-        while(left <= mid and right <= end) {
-            if(nums[left] < nums[right]) {
-                indices[nums[left].second] += nRightLessThanLeft;
-                temp[idx++] = nums[left++];
-            } else if(nums[left] > nums[right]) {
-                temp[idx++] = nums[right++];
-                nRightLessThanLeft++;
-            } else
-                left++, right++;
-        }
-        
-        while(left <= mid) {
-            indices[nums[left].second] += nRightLessThanLeft;
-            temp[idx++] = nums[left++];
-        }
-        
-        while(right <= end)
-            temp[idx++] = nums[right++];
-        
-        for(int i=start; i<=end; i++)
-            nums[i] = temp[i];
-    }
-    
 public:
     vector<int> countSmaller(vector<int>& nums) {
-        int n = nums.size();
-        vector<pair<int, int>> new_nums; // {num, original_idx}
-        vector<int> indices(n, 0);
-        vector<pair<int, int>> temp;
-        
-        for(int i=0; i<n; i++) {
-            new_nums.push_back({nums[i], i});
-            temp.push_back({nums[i], i});
+        vector<int> ans(nums.size());
+        ordered_set s;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            ans[i]=s.order_of_key({nums[i],i});
+            s.insert({nums[i],i});
         }
-        
-        merge_sort(0, n-1, new_nums, indices, temp);
-        
-        return indices;
+        return ans;
     }
 };
