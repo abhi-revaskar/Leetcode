d2ced0a8f504014fefe78deb8d6aaf1eaa9883ae
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-11 10:30:58 +0530

    Time: 35 ms (96.34%), Space: 89.3 MB (86.59%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
-   double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m=nums2.size();
-        
-        if(n>m)
-        return findMedianSortedArrays(nums2,nums1);
-
-        int l=0,h=n;
-        int mid;
-        int half=(n+m+1)/2;
-        while (l<=h)
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(),n = nums2.size();
+        if(m<n)
+            return findMedianSortedArrays(nums2,nums1);
+        int mid = (m+n+1)/2;
+        int low = mid-n , high = mid;
+        while(low<=high)
         {
-          mid=(l+h)/2;
-          int cut1=mid;
-          int cut2=half-mid;
-
-          int l1=cut1==0?INT_MIN:nums1[cut1-1];
-          int l2=cut2==0?INT_MIN:nums2[cut2-1];
-          int r1=cut1==n?INT_MAX:nums1[cut1];
-          int r2=cut2==m?INT_MAX:nums2[cut2];
-
-          if(l1<=r2 && l2<=r1)
-          {
-            return (n+m)&1?max(l1,l2):((max(l1,l2)+min(r1,r2))/2.0);
-          }
-          if(l1>r2)
-          {
-            h=mid-1;
-          }
-          else
-          {
-            l=mid+1;
-          }
-
+            int k = (low+high)/2;
+            int l1 = (k>0?nums1[k-1]:INT_MIN),r1 = (k<m?nums1[k]:INT_MAX);
+            int l2 = ((mid-k)>0?nums2[mid-k-1]:INT_MIN),r2 = ((mid-k)<n?nums2[mid-k]:INT_MAX);
+            if(l1>r2)
+                high = k-1;
+            else if(l2>r1)
+                low = k+1;
+            else
+            {
+                if((n+m)&1)
+                    return max(l1,l2);
+                else
+                    return (max(l1,l2)+min(r1,r2))/2.0;
+            }
         }
         return -1;
     }
