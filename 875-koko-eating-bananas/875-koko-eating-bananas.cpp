7704c5d0455cd01495fd5c89fb393355a8831d7e
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-12 19:06:27 +0530

    Time: 47 ms (92.54%), Space: 19 MB (31.56%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -9,20 +9,17 @@ public:
         }
         return hours<=h;
     }
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1,high = 0;
-        for(auto x:piles)
-        {
-            high = max(high,x);
+    int minEatingSpeed(vector<int>& piles, int H) {
+        int l = 1, r = 1000000000;
+        while (l < r) {
+            int m = (l + r) / 2, total = 0;
+            for (int p : piles)
+                total += (p + m - 1) / m;
+            if (total > H)
+                l = m + 1;
+            else
+                r = m;
         }
-        while(low<=high)
-        {
-            int mid = (low+high)/2;
-            if(isPossible(mid,piles,h))
-                high = mid-1;
-            else 
-                low = mid+1;
-        }
-        return low;
+        return l;
     }
 };
