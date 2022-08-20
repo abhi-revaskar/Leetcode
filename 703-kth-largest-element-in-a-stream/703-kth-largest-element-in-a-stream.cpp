498a831d426915c9e64ad2f8c29e790233d918fa
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-20 18:55:30 +0530

    Time: 33 ms (95.21%), Space: 19.8 MB (91.17%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -1,34 +1,22 @@
 class KthLargest {
 public:
-    priority_queue<int> q1;
-    priority_queue<int, vector<int>, greater<int>> q2;
+    priority_queue<int,vector<int>,greater<int>> pq;
     int K;
     KthLargest(int k, vector<int>& nums) {
-        int n = nums.size();
         K = k;
-        if(n>0)
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++)
+        for(auto x:nums)
         {
-            if(i<n-k)
-                q1.push(nums[i]);
-            else
-                q2.push(nums[i]);
+            pq.push(x);
+            if(pq.size()>k)
+                pq.pop();
         }
     }
     
     int add(int val) {
-        if(q2.size()<K)
-            q2.push(val);
-        else if(val >= q2.top())
-        {
-            q1.push(q2.top());
-            q2.pop();
-            q2.push(val);
-        }
-        else
-            q1.push(val);
-        return q2.top();
+        pq.push(val);
+        if(pq.size()>K)
+            pq.pop();
+        return pq.top();
     }
 };
 
