bb092077c162a2c461632b4d587756e59ed120fa
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-22 10:12:59 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (88.37%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool isPowerOfFour(long long n) {
-        if(n==0)
-            return false;
-        if((n&(n-1))==0 && (__builtin_popcount(n-1))%2==0)
+        if (n>0 && (n&(n-1))==0 && n&(0x55555555))
             return true;
         return false;
     }
