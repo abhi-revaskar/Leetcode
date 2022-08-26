8b3be1bcba984d58865ffc97f133cbe8922a3535
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-26 10:46:56 +0530

    Time: 4 ms (57.44%), Space: 10.8 MB (38.94%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    void solve(int i,vector<int> &ds,vector<vector<int>> &ans,vector<int> &nums)
+    void solve(int i,vector<vector<int>> &ans,vector<int> &subset,vector<int> &nums)
     {
         if(i==nums.size())
         {
-            ans.push_back(ds);
-            return ;
+            ans.push_back(subset);
+            return;
         }
-        solve(i+1,ds,ans,nums); // not pick
-        ds.push_back(nums[i]);
-        solve(i+1,ds,ans,nums); // pick
-        ds.pop_back();
+        // not pick
+        solve(i+1,ans,subset,nums);
+        //pick
+        subset.push_back(nums[i]);
+        solve(i+1,ans,subset,nums);
+        subset.pop_back();
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> ds;
         vector<vector<int>> ans;
-        solve(0,ds,ans,nums);
+        vector<int> subset;
+        solve(0,ans,subset,nums);
         return ans;
     }
 };
