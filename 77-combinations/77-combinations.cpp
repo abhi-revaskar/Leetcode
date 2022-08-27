893efec2b022a3b4667b3a05ef7d56bb40839d0b
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-27 13:16:14 +0530

    Time: 27 ms (84.31%), Space: 8.9 MB (91.33%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    void find(int i,vector<int> &ds,vector<vector<int>> &ans,int n,int k)
+    void solve(int i,int n,int k,vector<int> &ds,vector<vector<int>> &ans)
     {
-       if(k==0)
-       {
-           ans.push_back(ds);
-           return;      
-       }
-        if(i==n+1)
+        if(ds.size()>k)
+            return ;
+        if(ds.size()==k)
+        {
+            ans.push_back(ds);
             return;
-        find(i+1,ds,ans,n,k);
-        ds.push_back(i);
-        find(i+1,ds,ans,n,k-1);
-        ds.pop_back();
+        }
+        for(int j=i;j<=n;j++)
+        {
+            ds.push_back(j);
+            solve(j+1,n,k,ds,ans);
+            ds.pop_back();
+        }
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<int> ds;
         vector<vector<int>> ans;
-        find(1,ds,ans,n,k);
+        vector<int> ds;
+        solve(1,n,k,ds,ans);
         return ans;
     }
 };
