0ae14a37c1d9efe448828ac1d52b5d8865b9c90a
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-27 11:34:59 +0530

    Time: 1881 ms (44.21%), Space: 293.3 MB (47.23%) - LeetHub

---
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size(),m = matrix[0].size(),ans = INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                matrix[i][j]+=matrix[i][j-1];
+            }
+        }
+        for(int col1=0;col1<m;col1++)
+        {
+            for(int col2=col1;col2<m;col2++)
+            {
+                set<int> s;
+                s.insert(0);
+                int sum = 0;
+                for(int row = 0;row<n;row++)
+                {
+                    int x = matrix[row][col2]-(col1>0?matrix[row][col1-1]:0);
+                    sum+=x;
+                    // if(sum<k)
+                    //     ans = max(ans,sum);
+                    // if(sum-k==0)
+                    //     return k;
+                    auto it = s.lower_bound(sum-k);
+                    if(it!=s.end())
+                    {
+                        ans = max(ans,sum-*it);
+                        // cout<<ans<<endl;
+                    }
+                    s.insert(sum);
+                }
+            }
+        }
+        return ans;
+    }
+};
