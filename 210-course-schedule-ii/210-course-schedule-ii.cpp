76c711a90575154c99d9a1d9e3338426246bdacb
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-28 18:51:55 +0530

    Time: 26 ms (80.70%), Space: 14.3 MB (40.54%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -17,17 +17,43 @@ public:
     }
     vector<int> findOrder(int n, vector<vector<int>>& pre) {
         vector<vector<int>> adj(n);
+        vector<int> ind(n,0);
         for(auto x:pre)
         {
             adj[x[1]].push_back(x[0]);
+            ind[x[0]]++;
         }
-        vector<int> vis(n,0),done(n,0);
+        
+        //dfs
+        // vector<int> vis(n,0),done(n,0);
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(!vis[i] && dfs(i,adj,vis,done))
+        //         return {};
+        // }
+        // reverse(topo.begin(),topo.end());
+        // return topo;
+        
+        queue<int> q;
         for(int i=0;i<n;i++)
         {
-            if(!vis[i] && dfs(i,adj,vis,done))
-                return {};
+            if(!ind[i])
+                q.push(i);
         }
-        reverse(topo.begin(),topo.end());
+        while(!q.empty())
+        {
+            auto top = q.front();q.pop();
+            topo.push_back(top);
+            for(auto x:adj[top])
+            {
+                ind[x]--;
+                if(!ind[x])
+                    q.push(x);
+            }
+        }
+        if(topo.size()!=n)
+            return {};
         return topo;
+        
     }
 };
