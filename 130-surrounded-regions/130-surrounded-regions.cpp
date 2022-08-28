702faac94ec927e323d2fc40b52540f19234cb93
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-28 10:51:48 +0530

    Time: 17 ms (70.51%), Space: 9.8 MB (99.09%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector<vector<char>> &board)
+    void dfs(int i,int j,int n,int m,vector<vector<char>> &b)
     {
-        if(min(i,j)<0 || i==board.size() || j==board[0].size() || board[i][j]!='O')
+        if(min(i,j)<0 || i==n || j==m || b[i][j]!='O')
             return;
-        board[i][j] = 'N';
-        dfs(i+1,j,board);
-        dfs(i-1,j,board);
-        dfs(i,j+1,board);
-        dfs(i,j-1,board);
+        b[i][j] = 'N';
+        dfs(i+1,j,n,m,b);
+        dfs(i-1,j,n,m,b);
+        dfs(i,j-1,n,m,b);
+        dfs(i,j+1,n,m,b);
     }
     void solve(vector<vector<char>>& board) {
-        int m = board.size(),n=board[0].size();
-        for(int i=0;i<m;i++)
+        int n = board.size(),m= board[0].size();
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
-            {
-                if(board[i][j]=='O' && min(i,j)==0 || i==m-1 || j==n-1)
-                {
-                    dfs(i,j,board);
-                }
-                    
-            }
+            dfs(i,0,n,m,board);
+            dfs(i,m-1,n,m,board);
         }
         for(int i=0;i<m;i++)
         {
-            for(int j=0;j<n;j++)
+            dfs(0,i,n,m,board);
+            dfs(n-1,i,n,m,board);
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
             {
                 if(board[i][j]=='O')
-                {
-                    board[i][j]='X';
-                }
-                if(board[i][j]=='N')
-                    board[i][j]='O';
-                    
+                    board[i][j] = 'X';
+                else if(board[i][j]=='N')
+                    board[i][j] = 'O';
             }
         }
     }
