03d04c1265435833f6b2333d7e58b6ddb9562747
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-28 18:35:30 +0530

    Time: 33 ms (58.19%), Space: 14.2 MB (46.60%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -11,21 +11,45 @@ public:
                 return true;
         }
         done[i] = 1;
-        // cout<<i<<endl;
         return false;
     }
     bool canFinish(int n, vector<vector<int>>& pre) {
         vector<vector<int>> adj(n);
+        vector<int> ind(n,0);
         for(auto x:pre)
         {
             adj[x[1]].push_back(x[0]);
+            ind[x[0]]++;
         }
-        vector<int> vis(n,0),done(n,0);
+        //dfs
+        // vector<int> vis(n,0),done(n,0);
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(!vis[i] && dfs(i,adj,vis,done))
+        //         return false;
+        // }
+        // return true;
+        
+        //bfs
+        queue<int> q;
         for(int i=0;i<n;i++)
         {
-            if(!vis[i] && dfs(i,adj,vis,done))
-                return false;
+            if(!ind[i])
+                q.push(i);
         }
+        while(!q.empty())
+        {
+            auto top = q.front();q.pop();
+            for(auto x:adj[top])
+            {
+                ind[x]--;
+                if(!ind[x])
+                    q.push(x);
+            }
+        }
+        for(auto x:ind)
+            if(x!=0)
+                return false;
         return true;
     }
 };
