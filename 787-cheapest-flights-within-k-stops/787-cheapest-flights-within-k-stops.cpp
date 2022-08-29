a03902123af50ad88a1c0172c553735309d09f09
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-29 18:36:42 +0530

    Time: 56 ms (51.69%), Space: 16 MB (30.36%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -6,12 +6,19 @@ public:
         temp = dp;
         for(int i=0;i<=k;i++)
         {
+            int f = 0;
             for(auto x:flights)
             {
                 int u = x[0],v=x[1],w=x[2];
-                temp[v] = min(temp[v],dp[u]+w);
+                if(dp[u]+w<temp[v])
+                {
+                    temp[v] = min(temp[v],dp[u]+w);
+                    f =  1;                    
+                }
             }
             dp = temp;
+            if(!f)
+                break;
         }
         if(dp[dst]==1000000)
             return -1;
