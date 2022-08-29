18786346b77ef31c23d80bfdc8aebf4c86f72663
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-29 18:21:31 +0530

    Time: 241 ms (31.40%), Space: 43.7 MB (30.77%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<long long> dist(n+1,INT_MAX);
-        dist[k]=0;
-        for(int i=0;i<n;i++)
+        vector<int> dist(n+1,1000000);
+        dist[k] = 0;
+        vector<vector<pair<int,int>>> adj(n+1);
+        for(auto x:times)
         {
-            int f = 0;
-            for(auto edge:times)
+            adj[x[0]].push_back({x[1],x[2]});
+        }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.push({0,k});
+        while(!pq.empty())
+        {
+            auto top = pq.top();pq.pop();
+            int u = top.second;
+            for(auto nbr:adj[u])
             {
-                int u = edge[0],v = edge[1],w = edge[2];
+                // if(dist[u]!=top.first)
+                //     cout<<u<<" "<<dist[u]<<endl;
+                int v = nbr.first, w = nbr.second;
                 if(dist[u]+w<dist[v])
                 {
                     dist[v] = dist[u] + w;
-                    f = 1;
+                    pq.push({dist[v],v});
                 }
             }
-            if(!f)
-                break;
         }
-        long long ans = INT_MIN;
+        int ans = INT_MIN;
         for(int i=1;i<=n;i++)
             ans = max(ans,dist[i]);
-        if(ans == INT_MAX)
-            return -1;
-        return ans;
+        return ans==1000000?-1:ans;
     }
 };
