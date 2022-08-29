c497676b2ec3bce2124b0022665878c92c34b9b8
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-29 17:50:38 +0530

    Time: 1310 ms (28.63%), Space: 130.5 MB (27.64%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,57 +1,36 @@
-class DSU{
-    public:
-    vector<int> par,rank;
-    DSU(int n)
-    {
-        rank.resize(n,1);
-        par.resize(n);
-        for(int i=0;i<n;i++)
-        {
-            par[i]=i;
-        }
-    }
-    int find(int node)
-    {
-        if(par[node]==node)
-            return node;
-        return par[node] = find(par[node]);
-    }
-    void add(int a,int b)
-    {
-        int par_a = find(a);
-        int par_b = find(b);
-        if(rank[par_a]==rank[par_b])
-            rank[par_a]++;
-        if(rank[par_a]>rank[par_b])
-            par[par_b] = par_a;
-        else
-            par[par_a] = par_b;
-    }
-};
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        vector<pair<int,pair<int,int>>> adj;
-        for(int i=0;i<points.size();i++)
+        vector<vector<pair<int,int>>> adj(points.size());
+        int n = points.size();
+        for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<points.size();j++)
+            for(int j=i+1;j<n;j++)
             {
                 int dist = abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
-                adj.push_back({dist,{i,j}});
+                adj[i].push_back({j,dist});
+                adj[j].push_back({i,dist});                    
             }
         }
-        sort(adj.begin(),adj.end());
-        DSU *obj = new DSU(points.size());
         int ans = 0;
-        for(auto x:adj)
+        priority_queue<pair<int,int>> pq;
+        pq.push({0,0});
+        vector<bool> vis(n,0);
+        // vis[0]=1;
+        while(!pq.empty())
         {
-            int u = x.second.first,v=x.second.second;
-            if(obj->find(u)!=obj->find(v))
+            auto top = pq.top();pq.pop();
+            if(vis[top.second])
+                continue;
+            vis[top.second] = 1;
+            ans-=top.first;
+            for(auto nbr:adj[top.second])
             {
-                obj->add(u,v);
-                ans+=x.first;
+                if(!vis[nbr.first])
+                    pq.push({-nbr.second,nbr.first});
             }
         }
         return ans;
     }
 };
+ 
