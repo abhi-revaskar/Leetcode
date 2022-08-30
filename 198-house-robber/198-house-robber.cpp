f93f4c3c84f69b2b19867bc82a5fd3f57a442e64
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-30 11:30:45 +0530

    Time: 7 ms (12.72%), Space: 7.6 MB (77.23%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,12 +4,13 @@ public:
         int n = nums.size();
         if(n==1)
             return nums[0];
-        int dp[n+1];
-        dp[0] = 0,dp[1] = nums[0];
+        int prev2 = 0,prev1 = nums[0];
         for(int i=2;i<=n;i++)
         {
-            dp[i] = max(dp[i-1],nums[i-1]+dp[i-2]);
+            int curr = max(prev1,nums[i-1]+prev2);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n];
+        return prev1;
     }
 };
