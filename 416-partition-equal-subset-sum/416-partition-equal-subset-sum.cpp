947bfc3aaa3542b4746ef2f725d51690befeaa76
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-30 19:01:52 +0530

    Time: 497 ms (28.76%), Space: 94.3 MB (13.03%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -12,13 +12,27 @@ public:
         return dp[i][sum] = solve(i+1,sum,nums) || solve(i+1,sum-nums[i],nums);
     }
     bool canPartition(vector<int>& nums) {
-        int sum = 0;
-        memset(dp,-1,sizeof(dp));
+        int sum = 0,n=nums.size();
+        // memset(dp,-1,sizeof(dp));
         for(auto x:nums)
             sum+=x;
         if(sum&1)
             return false;
         sort(nums.begin(),nums.end());
-        return solve(0,sum/2,nums);
+        // return solve(0,sum/2,nums);
+        vector<vector<int>> dp(n+1,vector<int> (sum/2+1,0));
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++)
+        {
+            dp[i][0]=1;
+            for(int j=1;j<=sum/2;j++)
+            {
+                if(j<nums[i-1])
+                    dp[i][j] = dp[i-1][j]; //cant include
+                else
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
+            }
+        }
+        return dp.back().back();
     }
 };
