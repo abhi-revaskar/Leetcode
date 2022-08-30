3d442757b8443fbbd566bbf0bd56e918d8e6eb36
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-30 12:18:37 +0530

    Time: 21 ms (86.00%), Space: 10.3 MB (61.80%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,50 +1,48 @@
 class Solution {
 public:
     vector<int> manacher_odd(string &s)
-{
-  int n = s.length();
-  s = '$'+s+'*';
-  int l = 0,r = 1; //make l = 0
-  vector<int> ans(n+1);
-  for (int i = 1; i <=n ; i++)
-  {
-    ans[i] = min(r-i,ans[l+r-i]); // j = l+r-i , i will always be <=r
-    //find if ans for i can be increased above curr ans
-    while (s[i-ans[i]]==s[i+ans[i]])
     {
-      ans[i]++;
-    }
-    if(i+ans[i]>r)
-    {
-      r = i+ans[i];
-      l = i-ans[i];
-    }
-  }
-  return vector<int>(begin(ans)+1,end(ans));
-}
-    string longestPalindrome(string s) {
         string t;
         for(auto c:s)
         {
-            t+='#';
-            t+=c;
+            t+=string("#")+c;
         }
         t+='#';
-        auto d = manacher_odd(t);
-        int n = s.length();
-        string ans;
-        vector<int> odd(n),even(n);
-        for (int i = 0; i < n; i++)
+        t = '^'+t+'$';
+        int n = t.length(),l=0,r=1;
+        vector<int> d(n,0);
+        for(int i=1;i<n;i++)
         {
-            odd[i] = d[2*i+1]-1;
-            even[i] = (d[2*i]-1);
-            if(ans.length()<odd[i])
+            d[i] = min(d[l+r-i],r-i);
+            //trivial algo
+            while(t[i-d[i]]==t[i+d[i]])
+                d[i]++;
+            if(i+d[i]>r)
             {
-              ans = s.substr(i-odd[i]/2,odd[i]);
+                r = i+d[i];
+                l = i-d[i];
             }
-            if(ans.length()<even[i])
-                ans = s.substr(i-even[i]/2,even[i]);
         }
-        return ans;
+        return vector<int> (d.begin()+1,d.end()-1);
+    }
+    string longestPalindrome(string s) {
+        int n = s.length(),idx=0,len=1;
+        vector<int> d = manacher_odd(s),d1(n),d2(n);
+        for(int i=0;i<n;i++)
+        {
+            d1[i] = d[2*i+1]/2;
+            d2[i] = (d[2*i]-1)/2;
+            if(len<2*d1[i]-1)
+            {
+                len = 2*d1[i]-1;
+                idx = i-d1[i]+1;
+            }
+            if(len<2*d2[i])
+            {
+                len = 2*d2[i];
+                idx = i-d2[i];
+            }
+        }
+        return s.substr(idx,len);
     }
 };
