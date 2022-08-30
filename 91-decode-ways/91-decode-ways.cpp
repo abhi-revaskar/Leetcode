8e0e1b8268e4072d2dbb8168232681a7c65deae0
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-30 13:46:06 +0530

    Time: 5 ms (33.56%), Space: 6.1 MB (93.27%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     int dp[101];
-    int solve(string s)
+    bool check(int i,int n,string &s)
     {
-        if(dp[s.size()]!=-1)
-            return dp[s.size()];
-        int n = s.size();
-        // cout<<s<<endl;
-        if(s.length()<3)
-        {
-            if(s=="0" || s=="" || s[0] =='0')
-                return 0;
-            if(stoi(s)>0 && stoi(s)<=10 || stoi(s)==20)
-                return 1;
-            if(stoi(s)>=11 && stoi(s)<=19 || stoi(s)>20 && stoi(s)<=26)
-                return 2;
-        }
-        int ans = 0;
-        if(s.back()!='0')
-        ans += solve(s.substr(0,n-1));
-        if(stoi(s.substr(n-2))>9 && stoi(s.substr(n-2))<=26)
-            ans += solve(s.substr(0,n-2));
-        return dp[s.size()]=ans;
+        if(n>s.length())
+            return 0;
+        if(n==1)
+            return s[i]!='0';
+        if(i==s.length()-1)
+            return 0;
+        int code = stoi(s.substr(i,2));
+        return code>9 && code<27;
+    }
+    int solve(int i,string &s)
+    {
+        // cout<<i<<endl;
+        if(i>=s.length())
+            return 1;
+        if(dp[i]!=-1)
+            return dp[i];
+        return dp[i] = check(i,1,s)*solve(i+1,s)+check(i,2,s)*solve(i+2,s);
     }
     int numDecodings(string s) {
         memset(dp,-1,sizeof(dp));
-        return solve(s);
+        return solve(0,s);
     }
 };
