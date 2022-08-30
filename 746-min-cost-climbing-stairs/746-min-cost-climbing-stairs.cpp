94a982ade7ff184a3841b9afbd5a91a304b4bc9d
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-30 11:06:28 +0530

    Time: 11 ms (53.91%), Space: 13.7 MB (78.30%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -4,13 +4,14 @@ public:
         int n = cost.size();
         if(n==2)
             return min(cost[0],cost[1]);
-        int dp[n];
-        dp[n-1] = cost[n-1];
-        dp[n-2] = cost[n-2];
+        // int dp[n];
+        int next2 = cost[n-1],next1 = cost[n-2];
         for(int i=n-3;i>=0;i--)
         {
-            dp[i] = min(dp[i+1],dp[i+2])+cost[i];
+            int curr = min(next1,next2)+cost[i];
+            next2 = next1;
+            next1 = curr;
         }
-        return min(dp[0],dp[1]);
+        return min(next1,next2);
     }
 };
