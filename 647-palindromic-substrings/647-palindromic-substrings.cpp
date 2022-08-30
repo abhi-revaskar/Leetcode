077e4aaaf81d4e50b6da7dc9d5c7d79cd93ca17c
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-30 12:45:13 +0530

    Time: 3 ms (93.87%), Space: 6.9 MB (67.18%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
-   int pals(string s,int i,int j)
-   {
-       int ans = 0;
-       while(i>=0  && j<s.length())
-       {
-           if(s[i]!=s[j])
-               return ans;
-           ans++;
-           i--;j++;
-           
-       }
-       return ans;
-   }
-    int countSubstrings(string s) {
-        int ans = 0;
-        for(int i = 0;i<s.length();i++)
+    vector<int> manacher_odd(string &s)
+    {
+        string t;
+        for(auto c:s)
+            t+=string("#")+c;
+        t+='#';
+        t = '^'+t+'$';
+        int n = t.length(),l=0,r=1;
+        vector<int> d(n);
+        for(int i=1;i<n;i++)
         {
-            ans+=pals(s,i,i);
-            ans+=pals(s,i,i+1);
+            d[i] = min(r-i,d[l+r-i]);
+            while(t[i-d[i]]==t[i+d[i]])
+                d[i]++;
+            if(i+d[i]>r)
+            {
+                r = i+d[i];
+                l = i-d[i];
+            }
+        }
+        return vector<int> (begin(d)+1,end(d)-1);
+    }
+    int countSubstrings(string s) {
+        int n = s.length(),ans = 0;
+        vector<int> d = manacher_odd(s);
+        for(int i=0;i<n;i++)
+        {
+            ans+=d[2*i+1]/2;
+            ans+=(d[2*i]-1)/2;
         }
         return ans;
     }
