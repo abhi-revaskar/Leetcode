329c97f842de70069ffccbe9c5b0b8bef97b1c8e
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-30 18:54:31 +0530

    Time: 174 ms (80.29%), Space: 18.2 MB (48.15%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    int dp[201][20001];
-    bool solve(vector<int> &nums,int i,int s1,int s2)
+    int dp[201][10001];
+    bool solve(int i,int sum,vector<int> &nums)
     {
-        if(dp[i][s1]!=-1)
-            return dp[i][s1];
-        if(i==nums.size())
-            return s1==s2;
-        return dp[i][s1] = solve(nums,i+1,s1+nums[i],s2) || solve(nums,i+1,s1,s2+nums[i]);
+        if(sum==0)
+            return true;
+        if(i==nums.size() || nums[i]>sum)
+            return false;
+        if(dp[i][sum]!=-1)
+            return dp[i][sum];
+        return dp[i][sum] = solve(i+1,sum,nums) || solve(i+1,sum-nums[i],nums);
     }
     bool canPartition(vector<int>& nums) {
-        // int s1=0,s2=0;
-        // memset(dp,-1,sizeof(dp));
-        // return solve(nums,0,s1,s2);
         int sum = 0;
+        memset(dp,-1,sizeof(dp));
         for(auto x:nums)
             sum+=x;
         if(sum&1)
-            return 0;
-        vector<bool> dp(sum/2+1,0),prev(sum/2+1,0);
-        prev[0]=1;
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j = 1 ;j<=sum/2;j++)
-            {
-                if(nums[i]<=j)
-                    dp[j]=prev[j]||prev[j-nums[i]];
-            }
-            prev=dp;
-        }
-        return prev.back();
+            return false;
+        sort(nums.begin(),nums.end());
+        return solve(0,sum/2,nums);
     }
 };
