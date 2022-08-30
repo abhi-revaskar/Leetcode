4a3a881006a21d0ee5c979e784e3c1280c2322fc
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-30 10:27:55 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.47%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
     int dp[46];
-    int solve(int n,int i)
+    int solve(int n)
     {
-        if(i>n)
-            return 0;
-        if(dp[i]!=-1)
-            return dp[i];
-        if(n==1 || i==n)
-            return dp[i]=1;
-        return dp[i] = solve(n,i+1) + solve(n,i+2);
+        if(n==1 || n==2) return n;
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n] = solve(n-1)+solve(n-2);
     }
     int climbStairs(int n) {
-        // memset(dp,-1,sizeof(dp));
-        // return solve(n,0);
-        if(n==1 || n==2)
-            return n;
-        vector<int> dp(n+1,0);
-        dp[n-1]=1;
-        dp[n-2] = 2;
-        for(int i=n-3;i>=0;i--)
-        {
-            dp[i] = dp[i+1]+dp[i+2];
-        }
-        return dp[0];
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
     }
 };
