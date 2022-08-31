b1ff61b844691d80ebc4f11d06dc08bae6b4a8d6
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-31 13:43:01 +0530

    Time: 73 ms (25.17%), Space: 25.1 MB (6.58%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -13,7 +13,21 @@ public:
     }
     int change(int amt, vector<int>& coins) {
         sort(coins.begin(),coins.end());
-        memset(dp,-1,sizeof(dp));
-        return solve(0,amt,coins);
+        // memset(dp,-1,sizeof(dp));
+        // return solve(0,amt,coins);
+        int n = coins.size();
+        vector<vector<int>> dp(n+1,vector<int> (amt+1,0));
+        for(int i=1;i<=n;i++)
+        {
+            dp[i][0] = 1;
+            for(int j=1;j<=amt;j++)
+            {
+                if(j<coins[i-1])
+                    dp[i][j] = dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j]+dp[i][j-coins[i-1]];
+            }
+        }
+        return dp.back().back();
     }
 };
