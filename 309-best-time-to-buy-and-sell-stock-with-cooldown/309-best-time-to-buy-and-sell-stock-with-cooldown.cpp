dbdcbc91a48db24ee0af39faa5289214728f987d
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-31 12:53:58 +0530

    Time: 4 ms (80.62%), Space: 12 MB (25.35%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -15,7 +15,20 @@ public:
             return dp[i][buy] = max(solve(i+1,0,prices),solve(i+2,1,prices)+prices[i]);
     }
     int maxProfit(vector<int>& prices) {
-        memset(dp,-1,sizeof(dp));
-        return solve(0,1,prices);
+        // memset(dp,-1,sizeof(dp));
+        // return solve(0,1,prices);
+        int n = prices.size();
+        vector<vector<int>> dp(n+2,vector<int> (2,0));
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<2;j++)
+            {
+                if(j)
+                    dp[i][j] = max(dp[i+1][1],dp[i+1][0]-prices[i]);
+                else
+                    dp[i][j] = max(dp[i+1][0],dp[i+2][1]+prices[i]);
+            }
+        }
+        return dp[0][1];
     }
 };
