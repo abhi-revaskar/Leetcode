a5b40ba00493c9aec15da2eab2dd79ae230654c3
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-08-31 12:11:52 +0530

    Time: 33 ms (58.08%), Space: 10.9 MB (61.68%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.length(), m = text2.length();
-        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
-        for(int i=1;i<n+1;i++)
-        {
-            for(int j=1;j<m+1;j++)
-            {
-                if(text1[i-1]==text2[j-1])
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                else
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-        return dp[n][m];
+    int dp[1001][1001];
+    int solve(int i,int j,string &t1,string &t2)
+    {
+        if(i==t1.length() || j==t2.length())
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        if(t1[i]==t2[j])
+            return dp[i][j] = 1+solve(i+1,j+1,t1,t2);
+        return dp[i][j] = max(solve(i,j+1,t1,t2),solve(i+1,j,t1,t2));
     }
-}; 
+    int longestCommonSubsequence(string t1, string t2) {
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,t1,t2);
+    }
+};
