2c32396cc69b48ea25f62beb819a9577441ca27f
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-01 11:19:39 +0530

    Time: 83 ms (21.03%), Space: 10 MB (18.37%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -14,7 +14,23 @@ public:
         return dp[i][j] = 1+min(solve(i+1,j,s,t),min(solve(i+1,j+1,s,t),solve(i,j+1,s,t)));
     }
     int minDistance(string s, string t) {
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,s,t);
+        // memset(dp,-1,sizeof(dp));
+        // return solve(0,0,s,t);
+        int n=s.length(),m = t.length();
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        for(int i=0;i<=m;i++)
+            dp[0][i] = i;
+        for(int i=1;i<=n;i++)
+        {
+            dp[i][0]=i;
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==t[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = 1+min(dp[i-1][j],min(dp[i-1][j-1],dp[i][j-1]));
+            }
+        }
+        return dp.back().back();
     }
 };
