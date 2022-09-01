fe6e6b0eb5347458e3ca12b923bf179bd6883e9e
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-01 11:14:27 +0530

    Time: 129 ms (14.63%), Space: 7.2 MB (84.78%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size()+1,vector<int> (word2.size()+1,0));
-        for(int i = 0;i<=word1.size();i++)
-        {
-            dp[i][0]=i;
-            for(int j = 1;j<=word2.size();j++)
-            {
-                if(i==0)
-                    dp[i][j]=j;
-                else if(word1[i-1]!=word2[j-1])
-                    dp[i][j] = 1+min(dp[i-1][j],min(dp[i-1][j-1],dp[i][j-1]));
-                else
-                    dp[i][j] = dp[i-1][j-1];
-            }
-        }
-        return dp.back().back();
+    int dp[501][501];
+    int solve(int i,int j,string &s,string &t)
+    {
+        if(i==s.length())
+            return t.length()-j;
+        if(j==t.length())
+            return s.length()-i;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        if(s[i]==t[j])
+            return dp[i][j] = solve(i+1,j+1,s,t);
+        return dp[i][j] = 1+min(solve(i+1,j,s,t),min(solve(i+1,j+1,s,t),solve(i,j+1,s,t)));
+    }
+    int minDistance(string s, string t) {
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,s,t);
     }
 };
