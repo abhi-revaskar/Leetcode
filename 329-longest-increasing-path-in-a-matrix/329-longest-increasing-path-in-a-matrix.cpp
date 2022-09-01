c48a8bec165ccbe7291ad5a47e6733750a77a49c
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-01 09:48:13 +0530

    Time: 96 ms (32.58%), Space: 16 MB (67.77%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    int dp[201][201];
-    int solve(int i,int j,vector<vector<int>> &matrix)
+    int solve(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>& dp)
     {
         if(dp[i][j]!=-1)
             return dp[i][j];
-        int ans = 0,val = matrix[i][j];
-        if(i>0 && matrix[i-1][j]>val)
-            ans = max(ans,solve(i-1,j,matrix));
-        if(j>0 && matrix[i][j-1]>val)
-            ans = max(ans,solve(i,j-1,matrix));
-        if(i<matrix.size()-1 && matrix[i+1][j]>val)
-            ans = max(ans,solve(i+1,j,matrix));
-        if(j<matrix[0].size()-1 && matrix[i][j+1]>val)
-            ans = max(ans,solve(i,j+1,matrix));
+        int ans = 0,k = matrix[i][j];
+        if(i>0 && matrix[i-1][j]>k)
+            ans = max(ans,solve(i-1,j,matrix,dp));
+        if(i<matrix.size()-1 && matrix[i+1][j]>k)
+            ans = max(ans,solve(i+1,j,matrix,dp));
+        if(j>0 && matrix[i][j-1]>k)
+            ans = max(ans,solve(i,j-1,matrix,dp));
+        if(j<matrix[0].size()-1 && matrix[i][j+1]>k)
+            ans = max(ans,solve(i,j+1,matrix,dp));
         return dp[i][j] = ans+1;
     }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int ans = 0;
-        memset(dp,-1,sizeof(dp));
-        int n = matrix.size(),m = matrix[0].size();
+        int n = matrix.size(),m = matrix[0].size(),ans=1;
+        vector<vector<int>> dp(n,vector<int> (m,-1));
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                ans = max(ans,solve(i,j,matrix));
+                if(dp[i][j]==-1)
+                    ans = max(ans,solve(i,j,matrix,dp));
             }
         }
         return ans;
