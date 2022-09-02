d6694c3f7e1ebb5f3d06028b14197a9b508f35bf
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-02 11:08:22 +0530

    Time: 20 ms (81.94%), Space: 16.7 MB (53.94%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
-    int dp[10001];
-    int solve(vector<int> &nums,int i)
-    {
-        if(dp[i]!=-1)
-            return dp[i];
-        if(i==nums.size()-1)
-            return 0;
-        int n = nums.size();
-        int ans = 10001;
-        if(nums[i]==0)
-            return dp[i]=ans;
-        for(int j=1;j<=min(nums[i],n-i-1);j++)
-            ans = min(ans,1+solve(nums,i+j));
-        return dp[i]=ans;
-    }
     int jump(vector<int>& nums) {
-        // memset(dp,-1,sizeof(dp));
-        // return solve(nums,0);
-        if(nums.size()==1)
+        int start = 0,end = nums[0],n = nums.size(),ans=1;
+        if(n==1)
             return 0;
-        int maxreach = nums[0],i=1,ans = 1;
-        while(i<nums.size())
+        while(true)
         {
-            if(maxreach >= nums.size()-1)
-                return ans;
-            int t = maxreach;
-            while(i<=maxreach)
+            int newend = end;
+            for(int i=start;i<min(n,end+1);i++)
             {
-                t = max(t,nums[i]+i);
-                i++;
+                if(i==nums.size()-1)
+                    return ans;
+                newend = max(newend,i+nums[i]);
             }
-            if(t!=maxreach)
+            start = end+1;
+            end = newend;
             ans++;
-            maxreach = t;
         }
         return ans;
     }
