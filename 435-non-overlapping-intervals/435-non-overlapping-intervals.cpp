405254c7b59b078475880694d7516dda4b6863de
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-03 19:01:22 +0530

    Time: 938 ms (16.78%), Space: 89.9 MB (45.47%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -5,7 +5,7 @@ public:
         int i=0,ans=0;
         for(int j=1;j<ints.size();j++)
         {
-            auto x = ints[i],y = ints[j];
+            auto &x = ints[i],&y = ints[j];
             if(x[1]<=y[0])
                 i=j;
             else
