783ba927a5226d994d38ec17a994c76d71652866
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-05 18:34:00 +0530

    Time: 5 ms (29.34%), Space: 8.6 MB (85.35%) - LeetHub

---
+++ 66-plus-one/66-plus-one.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        reverse(digits.begin(),digits.end());
+        int carry = 1;
+        vector<int> ans;
+        for(auto x:digits)
+        {
+            int sum = x+carry;
+            ans.push_back(sum%10);
+            carry = sum/10;
+        }
+        if(carry)
+            ans.push_back(carry);
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
