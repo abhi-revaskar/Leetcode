858a83a66c7e1379baf95334ade0bbead06450b9
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-06 13:08:41 +0530

    Time: 1831 ms (5.04%), Space: 95.2 MB (44.44%) - LeetHub

---
+++ 2013-detect-squares/2013-detect-squares.cpp
@@ -0,0 +1,36 @@
+class DetectSquares {
+    unordered_map<int,unordered_multiset<int>> points;
+    vector<vector<int>> hori,vert;
+public:
+    DetectSquares() {
+        hori.resize(1001);
+        vert.resize(1001);
+    }
+    
+    void add(vector<int> point) {
+        int x = point[0], y = point[1];
+        points[x].insert(y);
+        hori[y].push_back(x);
+        vert[x].push_back(y);
+    }
+    
+    int count(vector<int> point) {
+        int ans = 0,x = point[0],y = point[1];
+        for(auto y1:vert[x])
+        {
+            int diff = y-y1;
+            if(diff==0)
+                continue;
+            ans+=(points[x-diff].count(y)*points[x-diff].count(y1));
+            ans+=(points[x+diff].count(y)*points[x+diff].count(y1));
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your DetectSquares object will be instantiated and called as such:
+ * DetectSquares* obj = new DetectSquares();
+ * obj->add(point);
+ * int param_2 = obj->count(point);
+ */
