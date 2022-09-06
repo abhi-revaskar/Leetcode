dffb3cf7a061137f62ceebea148ebb3b61c62fb4
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-06 13:47:23 +0530

    Time: 3 ms (97.31%), Space: 7.8 MB (72.63%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -2,19 +2,12 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> ans(n+1);
-        ans[0]=0;
+        ans[0] = 0;
         if(n==0)
             return ans;
-        ans[1]=1;
-        if(n==1)
-            return ans;
+        ans[1] = 1;
         for(int i=2;i<=n;i++)
-        {
-            if(i&1)
-                ans[i] = ans[i>>1]+1;
-            else
-                ans[i] = ans[i>>1];
-        }
+            ans[i] = ans[i>>1]+(i&1);
         return ans;
     }
 };
