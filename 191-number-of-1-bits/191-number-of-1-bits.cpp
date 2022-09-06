371c0a1b68429b177af47269d4790f19da2bd2e2
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-06 13:35:33 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (47.50%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        return __builtin_popcount(n);
+        // return __builtin_popcount(n);
+        int ans = 0;
+        while(n)
+        {
+            ans++;
+            n = n&(n-1);
+        }
+        return ans;
     }
 };
