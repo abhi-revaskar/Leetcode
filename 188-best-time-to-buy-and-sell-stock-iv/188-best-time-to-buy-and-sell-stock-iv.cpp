a38bfb685a479921cd92c743f8f2d2c2de5b9494
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-10 09:42:47 +0530

    Time: 18 ms (56.88%), Space: 11.6 MB (66.59%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int dp[1001][201];
-    int solve(int i,int trans,vector<int> &prices)
+    int dp[1001][2][101];
+    int solve(int i,int buy,int k,vector<int> &prices)
     {
-        if(dp[i][trans]!=-1)
-            return dp[i][trans];
-        if(i==prices.size() || trans == 0)
-            return dp[i][trans]= 0;
-        if(trans&1)
-            return dp[i][trans] =  max(prices[i]+solve(i+1,trans-1,prices),solve(i+1,trans,prices));
-        return dp[i][trans] = max(-prices[i]+solve(i+1,trans-1,prices),solve(i+1,trans,prices));
+        if(k==0 || i==prices.size())
+            return 0;
+        if(dp[i][buy][k]!=-1)
+            return dp[i][buy][k];
+        if(buy)
+            return dp[i][buy][k] = max(solve(i+1,0,k,prices)-prices[i],solve(i+1,1,k,prices));
+        else
+            return dp[i][buy][k] = max(solve(i+1,1,k-1,prices)+prices[i],solve(i+1,0,k,prices));
     }
     int maxProfit(int k, vector<int>& prices) {
         memset(dp,-1,sizeof(dp));
-        return solve(0,2*k,prices);
+        return solve(0,1,k,prices);
     }
 };
