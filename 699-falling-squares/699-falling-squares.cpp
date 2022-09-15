cc59ebb0899b588d6f4252c41db4b628c6162a46
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-15 18:52:22 +0530

    Time: 45 ms (84.78%), Space: 14.1 MB (26.63%) - LeetHub

--- 699-falling-squares/699-falling-squares.cpp
+++ 699-falling-squares/699-falling-squares.cpp
@@ -1,4 +1,41 @@
 class Solution {
+    vector<int> st,marked;
+    void push(int v)
+    {
+        if(marked[v])
+        {
+            st[2*v] = st[2*v+1] = st[v];
+            marked[2*v] = marked[2*v+1] = 1;
+            marked[v] = 0;
+        }
+    }
+    void update(int v,int tl,int tr,int l,int r,int val)
+    {
+        if(l>r)
+            return;
+        if(tl==l && tr==r)
+        {
+            st[v] = val;
+            marked[v] = 1;
+            return;
+        }
+        push(v);
+        int tm = (tl+tr)/2;
+        update(2*v,tl,tm,l,min(r,tm),val);
+        update(2*v+1,tm+1,tr,max(l,tm+1),r,val);
+        st[v] = max(st[2*v],st[2*v+1]);
+    }
+    
+    int query(int v,int tl,int tr,int l,int r)
+    {
+        if(l>r)
+            return 0;
+        if(tl==l && tr==r)
+            return st[v];
+        push(v);
+        int tm = (tl+tr)/2;
+        return max(query(2*v,tl,tm,l,min(r,tm)),query(2*v+1,tm+1,tr,max(l,tm+1),r));
+    }
     void add(int l,int r,int h,vector<int> &heights,int &maxh)
     {
         int t = 0;
@@ -23,13 +60,17 @@ public:
             heights.push_back(heights[i]);
         sort(begin(heights),end(heights));
         n = heights.size();
+        st.assign(4*n,0);
+        marked.assign(4*n,0);
         maxi.assign(n,0);
         for(auto x:positions)
         {
             int l = upper_bound(heights.begin(),end(heights),x[0])-begin(heights);
             int r = lower_bound(heights.begin(),end(heights),x[0]+x[1])-begin(heights);
             l--;
-            add(l,r,x[1],maxi,maxh);
+            int t = query(1,0,n-1,l,r);
+            update(1,0,n-1,l,r,t+x[1]);
+            maxh = max(maxh,t+x[1]);
             ans.push_back(maxh);
         }
         return ans;
