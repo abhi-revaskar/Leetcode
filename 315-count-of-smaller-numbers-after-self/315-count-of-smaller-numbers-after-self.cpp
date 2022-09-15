214bd3a946ce04aafaa3f5b5b774a0550a4b35cd
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-15 17:49:21 +0530

    Time: 881 ms (53.74%), Space: 94.6 MB (76.78%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,20 +1,47 @@
-#include <ext/pb_ds/assoc_container.hpp> // Common file
-#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
-#include <ext/pb_ds/detail/standard_policies.hpp>
-using namespace __gnu_pbds;
-
-
-
-typedef tree<pair<int,int>, null_type, less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 class Solution {
+    vector<int> st;
+    int n;
+    
+    void update(int v,int tl,int tr,int i)
+    {
+        if(tl==tr)
+        {
+            st[v]++;
+            return;
+        }
+        int tm = (tl+tr)/2;
+        if(i<=tm)
+            update(2*v,tl,tm,i);
+        else
+            update(2*v+1,tm+1,tr,i);
+        st[v] = st[2*v] + st[2*v+1];
+    }
+    
+    int sum(int v,int tl,int tr,int l,int r)
+    {
+        if(l>r)
+            return 0;
+        if(tl==l && tr==r)
+        {
+            return st[v];
+        }
+        int tm = (tr+tl)/2;
+        return sum(2*v,tl,tm,l,min(r,tm))+sum(2*v+1,tm+1,tr,max(l,tm+1),r);
+    }
 public:
     vector<int> countSmaller(vector<int>& nums) {
-        vector<int> ans(nums.size());
-        ordered_set s;
-        for(int i=nums.size()-1;i>=0;i--)
+        vector<int> sorted(nums),ans(nums.size());
+        n = nums.size();
+        st.resize(4*n,0);
+        sort(sorted.begin(),end(sorted));
+        for(int i=n-1;i>=0;i--)
         {
-            ans[i]=s.order_of_key({nums[i],i});
-            s.insert({nums[i],i});
+            int idx= lower_bound(sorted.begin(),sorted.end(),nums[i])-begin(sorted);
+            if(idx==0)
+                ans[i] = 0;
+            else
+                ans[i] = sum(1,0,n-1,0,idx-1);
+            update(1,0,n-1,idx);
         }
         return ans;
     }
