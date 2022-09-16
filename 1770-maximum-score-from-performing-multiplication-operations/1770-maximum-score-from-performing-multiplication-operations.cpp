cbb45f72e9d21ec3787fe1ef0c90c642c66dd53b
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-16 10:48:57 +0530

    Time: 415 ms (92.26%), Space: 54.6 MB (98.40%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,24 +1,24 @@
 class Solution {
-    const long long off = 1e9;
 public:
-    // unordered_map<int,unordered_map<int,int>> dp;
-    long long dp[1001][1001];
-    long long solve(int i,int k,int m,vector<int> &nums,vector<int> &mul)
+    int dp[1001][1001];
+    int solve(int i,int k,int m,vector<int> &nums,vector<int> &mul)
     {
         int j = nums.size()-1-k+i;
-        if(dp[i][k]!=-1)
-            return dp[i][k];
-        if(k==m-1)
+        if(k==m)
         {
-            return dp[i][k] = max(mul[k]*1LL*nums[i],mul[k]*1LL*nums[j])+off;
+            return 0;
         }
-        return dp[i][k] = off+max(solve(i+1,k+1,m,nums,mul)+mul[k]*1LL*nums[i],solve(i,k+1,m,nums,mul)+mul[k]*1LL*nums[j]);
+        if(dp[i][k]!=INT_MIN)
+            return dp[i][k];
+        return dp[i][k] = max(solve(i+1,k+1,m,nums,mul)+mul[k]*nums[i],solve(i,k+1,m,nums,mul)+mul[k]*nums[j]);
     }
     int maximumScore(vector<int>& nums, vector<int>& multipliers) {
         int n = nums.size(),m=multipliers.size();
-        memset(dp,-1,sizeof(dp));
-        long long ans = solve(0,0,m,nums,multipliers);
-        ans = ans-(off*1LL*m);
-        return ans;
+        for(int i=0;i<1001;i++)
+        {
+            for(int j=0;j<1001;j++)
+                dp[i][j] = INT_MIN;
+        }
+        return solve(0,0,m,nums,multipliers);
     }
 };
