224b72340bf55a4063c1aef1c5bdcb154a11e261
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-17 13:50:33 +0530

    Time: 1407 ms (24.55%), Space: 235.6 MB (69.85%) - LeetHub

--- 2080-range-frequency-queries/2080-range-frequency-queries.cpp
+++ 2080-range-frequency-queries/2080-range-frequency-queries.cpp
@@ -1,52 +1,11 @@
 class RangeFreqQuery {
-    unordered_map<int,unordered_map<int,int>> mp;
-    int n;
+    unordered_map<int, vector<int>> mp;
 public:
     RangeFreqQuery(vector<int>& arr) {
-        n = arr.size();
-        build(1,0,arr.size()-1,arr);
+        for(int i = 0; i < size(arr); i++) mp[arr[i]].push_back(i);
     }
-    void build(int v,int l,int r,vector<int> &arr)
-    {
-        if(l==r)
-        {
-            mp[v][arr[l]]++;
-            return;
-        }
-        int m = (l+r)/2;
-        build(2*v,l,min(r,m),arr);
-        build(2*v+1,max(l,m+1),r,arr);
-        for(auto x:mp[2*v])
-        {
-            if(mp[v].count(x.first))
-                mp[v][x.first]+=x.second;
-            else
-                mp[v].insert({x.first,x.second});
-        }
-        for(auto x:mp[2*v+1])
-        {
-            if(mp[v].count(x.first))
-                mp[v][x.first]+=x.second;
-            else
-                mp[v].insert({x.first,x.second});
-        }
-    }
-    int query(int v,int tl,int tr,int l,int r,int val)
-    {
-        if(l>r)
-            return 0;
-        if(tl==l && tr==r)
-            return mp[v][val];
-        int tm = (tl+tr)/2; 
-        return query(2*v,tl,tm,l,min(r,tm),val)+query(2*v+1,tm+1,tr,max(l,tm+1),r,val);
-    }
-    int query(int left, int right, int value) {
-        return query(1,0,n-1,left,right,value);
+    
+    int query(int L, int R, int V) {
+        return upper_bound(begin(mp[V]), end(mp[V]), R) - lower_bound(begin(mp[V]), end(mp[V]), L); 
     }
 };
-
-/**
- * Your RangeFreqQuery object will be instantiated and called as such:
- * RangeFreqQuery* obj = new RangeFreqQuery(arr);
- * int param_1 = obj->query(left,right,value);
- */
