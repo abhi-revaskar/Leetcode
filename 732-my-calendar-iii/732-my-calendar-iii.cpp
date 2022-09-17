9d8bdf4e2114e95fb9ca6e19ea885df69da2ba0a
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-17 12:34:30 +0530

    Time: 1340 ms (5.00%), Space: 335.2 MB (5.00%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.cpp
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -1,77 +1,39 @@
-struct node{
-    int l,r,lazy,val;
-    node *left,*right;
-    node(int l,int r,int val)
-    {
-        this->l = l;
-        this->r = r;
-        this->val = val;
-        lazy = 0;
-        left = right = NULL;
-    }
-};
 class MyCalendarThree {
-    map<int,int> mp;
-    node *root;
+    unordered_map<int,int> val,lazy;
 public:
     MyCalendarThree() {
-        root = new node(0,1e9,0);
+        
     }
-    void push(node *root)
+    void push(int v)
     {
-        if(!root->left)
+        if(lazy[v])
         {
-            int m = (root->l+root->r)/2;
-            root->left = new node(root->l,m,root->val);
-            root->right = new node(m+1,root->r,root->val);
+            val[2*v]+=lazy[v];
+            val[2*v+1]+=lazy[v];
+            lazy[2*v]+=lazy[v];
+            lazy[2*v+1]+=lazy[v];
+            lazy[v] = 0;
         }
-        else if(root->lazy)
-        {
-            root->left->val+=root->lazy;
-            root->right->val+=root->lazy;
-            root->left->lazy+=root->lazy;
-            root->right->lazy+=root->lazy;
-        }
-        root->lazy = 0;
     }
-    void update(node *root,int l,int r,int val)
+    void update(int v,int tl,int tr,int l,int r)
     {
         if(l>r)
             return;
-        if(root->l==l &&root->r == r)
+        if(tl==l && tr==r)
         {
-            root->val+=val;
-            root->lazy+=val;
+            val[v]++;
+            lazy[v]++;
             return;
         }
-        push(root);
-        int m = (root->l+root->r)/2;
-        update(root->left,l,min(r,m),val);
-        update(root->right,max(l,m+1),r,val);
-        root->val = max(root->left->val,root->right->val);
-    }
-    int query(node *root,int l,int r)
-    {
-        if(l>r)
-            return 0;
-        if(root->l==l && root->r==r)
-            return root->val;
-        push(root);
-        int m = (root->l+root->r)/2;
-        return max(query(root->left,l,min(r,m)),query(root->right,max(l,m+1),r));
+        int tm = (tl+tr)/2;
+        push(v);
+        update(2*v,tl,tm,l,min(r,tm));
+        update(2*v+1,tm+1,tr,max(l,tm+1),r);
+        val[v] = max(val[2*v],val[2*v+1]);
     }
     int book(int start, int end) {
-        // update(root,start,end-1,1);
-        // return root->val;
-        int ans = 0,sum=0;
-        mp[start]++;
-        mp[end]--;
-        for(auto x:mp)
-        {
-            sum+=x.second;
-            ans = max(ans,sum);
-        }
-        return ans;
+        update(1,0,1e9,start,end-1);
+        return val[1];
     }
 };
 
