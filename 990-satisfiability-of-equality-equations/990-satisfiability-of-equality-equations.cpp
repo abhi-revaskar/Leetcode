3ad10c649bca4c46121fd9bcbc34e5fdf8b972a6
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-09-26 09:29:17 +0530

    Time: 10 ms (57.64%), Space: 11.4 MB (44.49%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,50 +1,50 @@
-class DSU{
+class dsu{
     public:
-    vector<int> rank,parent;
-    DSU()
+    vector<int> par,rank;
+    dsu(int n)
     {
-        rank.resize(26,1);
-        parent.resize(26);
-        for(int i=0;i<26;i++)
-            parent[i]=i;
-    }
-    int find(int n)
-    {
-        if(parent[n]==n)
-            return n;
-        return parent[n]=find(parent[n]);
+        par.resize(n,0);
+        rank.resize(n,1);
+        for(int i=0;i<n;i++)
+            par[i] = i;
     }
     void add(int a,int b)
     {
         int par_a = find(a);
         int par_b = find(b);
-        if(rank[par_a]>rank[par_b])
+        if(rank[par_a]==rank[par_b])
         {
-            parent[par_b] = par_a;
+            rank[par_a]++;
+            par[par_b] = par_a;
         }
-        else if(rank[par_a]<rank[par_b])
+        else if(rank[par_a]>rank[par_b])
         {
-            parent[par_a] = par_b;
+            par[par_b] = par_a;
         }
         else
-        {
-            parent[par_b] = par_a;
-            rank[par_a]++;
-        }
+            par[par_a] = par_b;
+    }
+    int find(int n)
+    {
+        if(par[n]==n)
+            return n;
+        return par[n] = find(par[n]);
     }
 };
 class Solution {
 public:
     bool equationsPossible(vector<string>& equations) {
-        DSU *obj = new DSU();
+        dsu *obj = new dsu(26);
         for(auto x:equations)
         {
             if(x[1]=='=')
+            {
                 obj->add(x[0]-'a',x[3]-'a');
+            }
         }
         for(auto x:equations)
         {
-            if(x[1]=='!' && (obj->find(x[0]-'a'))==(obj->find(x[3]-'a')))
+            if(x[1]=='!' && obj->find(x[0]-'a')==obj->find(x[3]-'a'))
                 return false;
         }
         return true;
