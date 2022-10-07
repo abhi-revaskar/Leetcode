b547836e33d74f6ef3d8e6cb7f7e5c67153ecb7d
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-10-07 09:57:25 +0530

    Time: 1537 ms (5.03%), Space: 335.2 MB (5.03%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.cpp
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -1,39 +1,45 @@
 class MyCalendarThree {
-    unordered_map<int,int> val,lazy;
-public:
-    MyCalendarThree() {
-        
-    }
+    unordered_map<int,int> st,lazy;
     void push(int v)
     {
         if(lazy[v])
         {
-            val[2*v]+=lazy[v];
-            val[2*v+1]+=lazy[v];
             lazy[2*v]+=lazy[v];
             lazy[2*v+1]+=lazy[v];
-            lazy[v] = 0;
+            st[2*v]+=lazy[v];
+            st[2*v+1]+=lazy[v];
+            lazy[v] = 0;     
         }
     }
     void update(int v,int tl,int tr,int l,int r)
     {
-        if(l>r)
-            return;
+        if(l>r) return ;
         if(tl==l && tr==r)
         {
-            val[v]++;
             lazy[v]++;
+            st[v]++;
             return;
         }
-        int tm = (tl+tr)/2;
         push(v);
+        int tm = (tl+tr)/2;
         update(2*v,tl,tm,l,min(r,tm));
-        update(2*v+1,tm+1,tr,max(l,tm+1),r);
-        val[v] = max(val[2*v],val[2*v+1]);
+        update(2*v+1,tm+1,tr,max(tm+1,l),r);
+        st[v] = max(st[2*v],st[2*v+1]);
     }
+    int query(int v,int l,int r)
+    {
+        push(v);
+        return st[v];
+    }
+public:
+    MyCalendarThree() {
+        
+    }
+    
     int book(int start, int end) {
         update(1,0,1e9,start,end-1);
-        return val[1];
+        int ans = query(1,0,1e9);
+        return ans;
     }
 };
 
