97562fc440fb76f963fe67d06e075b1174a4f93b
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-10-19 10:43:58 +0530

    Time: 37 ms (13.25%), Space: 12.6 MB (76.98%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,3 +1,13 @@
+class Compare
+{
+public:
+    bool operator() (pair<int,string> &a, pair<int,string> &b)
+    {
+        if(a.first!=b.first)
+            return a.first>b.first;
+        return a.second<b.second;
+    }
+};
 class Solution {
 public:
     static bool comp(pair<int,string> &a,pair<int,string> &b)
@@ -11,15 +21,23 @@ public:
         unordered_map<string,int> mp;
         for(auto &x:words)
             mp[x]++;
+        priority_queue<pair<int,string>,vector<pair<int,string>>,Compare> pq;
         for(auto &x:mp)
         {
-            ans.push_back({x.second,x.first});
+            pq.push({x.second,x.first});
+            // cout<<"added "<<x.first<<endl;
+            // cout<<pq.top().second<<endl;
+            if(pq.size()>k)
+                pq.pop();
         }
-        sort(begin(ans),end(ans),comp);
-        // reverse(begin(ans),end(ans));
         vector<string> ret;
-        for(int i=0;i<k;i++)
-            ret.push_back(ans[i].second);
+        while(!pq.empty())
+        {
+            // cout<<pq.top().second<<endl;
+            ret.push_back(pq.top().second);
+            pq.pop();
+        }
+        reverse(begin(ret),end(ret));
         return ret;
     }
 };
