b40fcb119d1d77e5a5389cdeef9709c5a2ba9781
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-11-02 11:28:08 +0530

    Time: 4 ms (29.93%), Space: 6.6 MB (81.49%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
-    int solve(string st,string end,unordered_set<string> &s)
-    {
-        if(st==end)
-            return 0;
-        int ans = 1000;
-        vector<char> v{'A','C','G','T'};
-        for(int i=0;i<8;i++)
+    int minMutation(string start, string end, vector<string>& bank) {
+        unordered_set<string> vis,s;
+        queue<string> q;
+        q.push(start);
+        vis.insert(start);
+        for(auto &x:bank)
+            s.insert(x);
+        int steps=0;
+        while(!q.empty())
         {
-            for(auto x:v)
+            int sz = q.size();
+            while(sz--)
             {
-                if(x!=st[i])
+                string t = q.front();q.pop();
+                if(t==end)
+                    return steps;
+                for(int i=0;i<8;i++)
                 {
-                    string t = st;
-                    t[i] = x;
-                    if(s.count(t))
+                    for(auto x:"ACGT")
                     {
-                        s.erase(t);
-                        ans = min(ans,solve(t,end,s));
-                        s.insert(t);
+                        string nbr=t;
+                        nbr[i]=x;
+                        if(vis.count(nbr)==0 && s.count(nbr))
+                        {
+                            vis.insert(nbr);
+                            q.push(nbr);
+                        }
                     }
                 }
             }
+            steps++;
         }
-        return ans+1;
-    }
-    int minMutation(string start, string end, vector<string>& bank) {
-        unordered_set<string> s;
-        for(auto &x:bank)
-        {
-            s.insert(x);
-        }
-        if(s.count(end)==0)
-            return -1;
-        int ans = solve(start,end,s);
-        if(ans>10)
-            return -1;
-        return ans;
+        return -1;
             
     }
 };
