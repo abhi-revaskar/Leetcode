dc56390f94abba0bf8315c16c23c264ebab541c0
Abhishek Revaskar <106136067+abhi-revaskar@users.noreply.github.com>
2022-11-03 09:44:54 +0530

    Time: 1726 ms (5.15%), Space: 211.6 MB (5.22%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        unordered_multiset<string> s;
+        int len = 0;
+        for(auto &x:words)
+        {
+            string t = x;
+            reverse(begin(t),end(t));
+            if(s.count(t))
+            {
+                len+=4;
+                s.erase(s.find(t));
+            }
+            else
+                s.insert(x);
+        }
+        for(auto &x:s)
+        {
+            if(x[0]==x[1])
+            {
+                // cout<<"y";
+                len+=2;
+                break;
+            }
+        }
+        return len;
+    }
+};
